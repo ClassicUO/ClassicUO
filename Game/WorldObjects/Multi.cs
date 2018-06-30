ef71b2ebe20d727f3b90103c23dfd2b80c4b46cf
Andrea Angelini <andrea-angelini@outlook.it>
2018-06-30 20:05:59 +0200

    + multi

---
+++ Game/WorldObjects/Multi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicUO.Game.WorldObjects
+{
+    public class Multi : WorldObject
+    {
+        public Multi()
+        {
+
+        }
+
+        public override Position Position { get; set; }
+    }
+}
