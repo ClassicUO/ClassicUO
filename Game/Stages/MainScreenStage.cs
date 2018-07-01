5df12147bb23e233e416147131a2ee2d52e4feeb
Andrea Angelini <andrea-angelini@outlook.it>
2018-07-01 16:59:58 +0200

    + stages

---
+++ Game/Stages/MainScreenStage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicUO.Game.Stages
+{
+    public sealed class MainScreenStage : Stage
+    {
+        public MainScreenStage()
+        {
+
+        }
+
+        private bool OnBeforeLogin()
+        {
+            return true;
+        }
+
+        private bool OnLogin()
+        {
+            return true;
+        }
+
+        private bool OnLoginFailed()
+        {
+            return true;
+        }
+
+        private bool OnLoginAccepted()
+        {
+            return true;
+        }
+
+        private bool OnServerList()
+        {
+            return true;
+        }
+
+        private bool OnServerListSelected()
+        {
+            return true;
+        }
+
+        private bool OnCharacterList()
+        {
+            return true;
+        }
+
+        private bool OnCharacterListSelected()
+        {
+            return true;
+        }
+
+        private bool OnCharacterDeleted()
+        {
+            return true;
+        }
+
+        private bool OnCharacterCreated()
+        {
+            return true;
+        }
+
+        private bool OnWorldEntered()
+        {
+            return true;
+        }
+    }
+}
