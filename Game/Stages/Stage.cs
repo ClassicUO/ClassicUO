a080edd206b805ab97642c9cca500e8a7a32a3d6
Andrea Angelini <andrea-angelini@outlook.it>
2018-07-01 12:02:47 +0200

    + stages

---
+++ Game/Stages/Stage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicUO.Game.Stages
+{
+    public abstract class Stage
+    {
+        protected Stage()
+        {
+            ChainActions = new List<Func<bool>>();
+        }
+
+        public List<Func<bool>> ChainActions { get; }
+
+
+        public virtual void Load()
+        {
+
+        }
+
+        public virtual void Unload()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+    }
+}
---
+++ Game/Stages/StartStage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicUO.Game.Stages
+{
+    public sealed class StartStage : Stage
+    {
+        public StartStage()
+        {
+            ChainActions.Add(OnGameLoading);
+            ChainActions.Add(OnGameLoaded);
+        }
+
+
+        private bool OnGameLoading()
+        {
+            return true;
+        }
+
+        private bool OnGameLoaded()
+        {
+            return true;
+        }
+    }
+}
