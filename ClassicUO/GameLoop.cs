0f889adbc62a681ccc307c52c8e0b25b9edb7ebb
Andrea Angelini <andrea-angelini@outlook.it>
2018-07-06 18:43:11 +0200

    + mouse load textures

--- ClassicUO/GameLoop.cs
+++ ClassicUO/GameLoop.cs
@@ -69,6 +69,7 @@ namespace ClassicUO
             PacketHandlers.Load();
             PacketsTable.AdjustPacketSizeByVersion(AssetsLoader.FileManager.ClientVersion);
 
+            _mouseManager.LoadTextures();
 
             //NetClient.Socket.Connect("login.uodemise.com", 2593);
 
--- Input/MouseManager.cs
+++ Input/MouseManager.cs
@@ -1,5 +1,6 @@
 ﻿using ClassicUO.Utility;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 
@@ -9,10 +10,150 @@ namespace ClassicUO.Input
     {
         private MouseState _prevMouseState = Mouse.GetState();
 
+        private readonly ushort[,] _cursorData = new ushort[2, 16]
+        {
+            {
+                0x206A, 0x206B, 0x206C, 0x206D, 0x206E, 0x206F,
+                0x2070, 0x2071, 0x2072, 0x2073, 0x2074, 0x2075,
+                0x2076, 0x2077, 0x2078, 0x2079
+            },
+            {
+                0x2053, 0x2054, 0x2055, 0x2056, 0x2057, 0x2058,
+                0x2059, 0x205A, 0x205B, 0x205C, 0x205D, 0x205E,
+                0x205F, 0x2060, 0x2061,
+                0x2062
+            }
+        };
+
+        private readonly int[,] _cursorOffset = new int[2, 16];
+
         public MouseManager(Game game) : base(game)
         {
            
         }
+
+
+        public event EventHandler<MouseEventArgs> MouseDown, MouseUp, MouseMove;
+        public event EventHandler<MouseWheelEventArgs> MouseWheel;
+
+
+        public void LoadTextures()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    ushort id = _cursorData[i, j];
+                    ushort[] pixels = AssetsLoader.Art.ReadStaticArt(id, out short width, out short height);
+                    Texture2D texture = new Texture2D(this.Game.GraphicsDevice, width, height, false, SurfaceFormat.Bgra5551);
+                    texture.SetData(pixels);
+
+                    if (i == 0)
+                    {
+                        if (texture != null)
+                        {
+                            float offX = 0;
+                            float offY = 0;
+
+                            float dw = texture.Width;
+                            float dh = texture.Height;
+
+                            if (id == 0x206A)
+                                offX = -4f;
+                            else if (id == 0x206B)
+                                offX = -dw + 3f;
+                            else if (id == 0x206C)
+                            {
+                                offX = -dw + 3f;
+                                offY = -(dh / 2f);
+                            }
+                            else if (id == 0x206D)
+                            {
+                                offX = -dw;
+                                offY = -dh;
+                            }
+                            else if (id == 0x206E)
+                            {
+                                offX = -(dw * 0.66f);
+                                offY = -dh;
+                            }
+                            else if (id == 0x206F)
+                                offY = ((-dh) + 4f);
+                            else if (id == 0x2070)
+                                offY = ((-dh) + 4f);
+                            else if (id == 0x2075)
+                                offY = -4f;
+                            else if (id == 0x2076)
+                            {
+                                offX = -12f;
+                                offY = -14f;
+                            }
+                            else if (id == 0x2077)
+                            {
+                                offX = -(dw / 2f);
+                                offY = -(dh / 2f);
+                            }
+                            else if (id == 0x2078)
+                                offY = -(dh * 0.66f);
+                            else if (id == 0x2079)
+                                offY = -(dh / 2f);
+
+                            switch (id)
+                            {
+                                case 0x206B:
+                                    offX = -29;
+                                    offY = -1;
+                                    break;
+                                case 0x206C:
+                                    offX = -41;
+                                    offY = -9;
+                                    break;
+                                case 0x206D:
+                                    offX = -36;
+                                    offY = -25;
+                                    break;
+                                case 0x206E:
+                                    offX = -14;
+                                    offY = -33;
+                                    break;
+                                case 0x206F:
+                                    offX = -2;
+                                    offY = -26;
+                                    break;
+                                case 0x2070:
+                                    offX = -3;
+                                    offY = -8;
+                                    break;
+                                case 0x2071:
+                                    offX = -1;
+                                    offY = -1;
+                                    break;
+                                case 0x206A:
+                                    offX = -4;
+                                    offY = -2;
+                                    break;
+                                case 0x2075:
+                                    offX = -2;
+                                    offY = -10;
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            _cursorOffset[0, j] = (int)offX;
+                            _cursorOffset[1, j] = (int)offY;
+                        }
+                        else
+                        {
+                            _cursorOffset[0, j] = 0;
+                            _cursorOffset[1, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+
 
         public override void Update(GameTime gameTime)
         {
@@ -90,9 +231,7 @@ namespace ClassicUO.Input
             base.Update(gameTime);
         }
 
-        public event EventHandler<MouseEventArgs> MouseDown, MouseUp, MouseMove;
-        public event EventHandler<MouseWheelEventArgs> MouseWheel;
-
+       
         private bool IsMouseButtonDown(ButtonState current, ButtonState prev) => current == ButtonState.Pressed && prev == ButtonState.Released;
         private bool IsMouseButtonUp(ButtonState current, ButtonState prev) => current == ButtonState.Released && prev == ButtonState.Pressed;
     }
