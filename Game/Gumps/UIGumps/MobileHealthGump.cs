3d7cb307947a5b7141b8c3ad7d3fd5227bf8456e
Andrea Angelini <andrea-angelini@outlook.it>
2018-11-13 21:36:18 +0100

    + crash healthbar

--- Game/Gumps/UIGumps/MobileHealthGump.cs
+++ Game/Gumps/UIGumps/MobileHealthGump.cs
@@ -88,7 +88,8 @@ namespace ClassicUO.Game.Gumps.UIGumps
             _mobile.ManaChanged += MobileOnManaChanged;
             _mobile.StaminaChanged += MobileOnStaminaChanged;
 
-            _textboxName.AcceptMouseInput = false;
+            if (_textboxName != null)
+                _textboxName.AcceptMouseInput = false;
 
         }
 
