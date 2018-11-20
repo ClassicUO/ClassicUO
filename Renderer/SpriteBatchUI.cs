9e8ab4e41aeea6c1eb48ef7bcb67b0810bab6a8b
Andrea Angelini <andrea-angelini@outlook.it>
2018-11-20 13:56:24 +0100

    only for testing purpose

--- Renderer/SpriteBatchUI.cs
+++ Renderer/SpriteBatchUI.cs
@@ -224,52 +224,52 @@ namespace ClassicUO.Renderer
 
         public unsafe bool Draw2DTiled(Texture2D texture, Rectangle destRect, Vector3 hue)
         {
-            //float drawCountX = destRect.Width / (float) texture.Width;
-            //float drawCountY = destRect.Height / (float)texture.Height;
+            /* float drawCountX = destRect.Width / (float)texture.Width;
+            float drawCountY = destRect.Height / (float)texture.Height;
 
-            //fixed (SpriteVertex* ptr = _vertexBufferUI)
-            //{
-            //    ptr[0].Position.X = destRect.X;
-            //    ptr[0].Position.Y = destRect.Y;
-            //    ptr[0].Position.Z = 0;
-            //    ptr[0].Normal.X = 0;
-            //    ptr[0].Normal.Y = 0;
-            //    ptr[0].Normal.Z = 1;
-            //    ptr[0].TextureCoordinate = Vector3.Zero;
+            fixed (SpriteVertex* ptr = _vertexBufferUI)
+            {
+                ptr[0].Position.X = destRect.X;
+                ptr[0].Position.Y = destRect.Y;
+                ptr[0].Position.Z = 0;
+                ptr[0].Normal.X = 0;
+                ptr[0].Normal.Y = 0;
+                ptr[0].Normal.Z = 1;
+                ptr[0].TextureCoordinate = Vector3.Zero;
 
-            //    ptr[1].Position.X = destRect.Right;
-            //    ptr[1].Position.Y = destRect.Y;
-            //    ptr[1].Position.Z = 0;
-            //    ptr[1].Normal.X = 0;
-            //    ptr[1].Normal.Y = 0;
-            //    ptr[1].Normal.Z = 1;
-            //    ptr[1].TextureCoordinate.X = drawCountX;
-            //    ptr[1].TextureCoordinate.Y = 0;
-            //    ptr[1].TextureCoordinate.Z = 0;
+                ptr[1].Position.X = destRect.Right;
+                ptr[1].Position.Y = destRect.Y;
+                ptr[1].Position.Z = 0;
+                ptr[1].Normal.X = 0;
+                ptr[1].Normal.Y = 0;
+                ptr[1].Normal.Z = 1;
+                ptr[1].TextureCoordinate.X = drawCountX;
+                ptr[1].TextureCoordinate.Y = 0;
+                ptr[1].TextureCoordinate.Z = 0;
 
-            //    ptr[2].Position.X = destRect.X;
-            //    ptr[2].Position.Y = destRect.Bottom;
-            //    ptr[2].Position.Z = 0;
-            //    ptr[2].Normal.X = 0;
-            //    ptr[2].Normal.Y = 0;
-            //    ptr[2].Normal.Z = 1;
-            //    ptr[2].TextureCoordinate.X = 0;
-            //    ptr[2].TextureCoordinate.Y = 1;
-            //    ptr[2].TextureCoordinate.Z = 0;
+                ptr[2].Position.X = destRect.X;
+                ptr[2].Position.Y = destRect.Bottom;
+                ptr[2].Position.Z = 0;
+                ptr[2].Normal.X = 0;
+                ptr[2].Normal.Y = 0;
+                ptr[2].Normal.Z = 1;
+                ptr[2].TextureCoordinate.X = 0;
+                ptr[2].TextureCoordinate.Y = drawCountY;
+                ptr[2].TextureCoordinate.Z = 0;
 
-            //    ptr[3].Position.X = destRect.Right;
-            //    ptr[3].Position.Y = destRect.Bottom;
-            //    ptr[3].Position.Z = 0;
-            //    ptr[3].Normal.X = 0;
-            //    ptr[3].Normal.Y = 0;
-            //    ptr[3].Normal.Z = 1;
-            //    ptr[3].TextureCoordinate.X = 1;
-            //    ptr[3].TextureCoordinate.Y = 1;
-            //    ptr[3].TextureCoordinate.Z = 0;
-            //    ptr[0].Hue = ptr[1].Hue = ptr[2].Hue = ptr[3].Hue = hue;
-            //}
+                ptr[3].Position.X = destRect.Right;
+                ptr[3].Position.Y = destRect.Bottom;
+                ptr[3].Position.Z = 0;
+                ptr[3].Normal.X = 0;
+                ptr[3].Normal.Y = 0;
+                ptr[3].Normal.Z = 1;
+                ptr[3].TextureCoordinate.X = drawCountX;
+                ptr[3].TextureCoordinate.Y = drawCountY;
+                ptr[3].TextureCoordinate.Z = 0;
+                ptr[0].Hue = ptr[1].Hue = ptr[2].Hue = ptr[3].Hue = hue;
+            }
 
-            //return DrawSprite(texture, _vertexBufferUI);
+            return DrawSprite(texture, _vertexBufferUI); */
 
             int y = destRect.Y;
             int h = destRect.Height;
