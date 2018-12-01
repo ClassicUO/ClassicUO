aeae181202ed9b3ef2b1e12e7fc553f879d487ef
fwiffo <fwiffo@fwiffo.net>
2018-12-01 19:27:46 +0100

    UltimaLive: - Chunks, if non present, are preloaded (this, in case of sudden teleporting to a different facet, that won't allow the client to precache chunks, since usually those chunks are already there) - reordered calls, to prevent eventual crashes if out of bounds or in anomalies in server packets - Created a GetMapChunk() method in facet, to preload what is needed inside ultimalive
    this should completely fix any issue, now the missing thing is only file-write on disk, in case of modifications sent by the server.

--- src/Game/Map/Facet.cs
+++ src/Game/Map/Facet.cs
@@ -31,7 +31,7 @@ namespace ClassicUO.Game.Map
 {
     public sealed class Facet : IDisposable
     {
-       // private static Tile _invalid = Tile.Invalid;
+        // private static Tile _invalid = Tile.Invalid;
         private readonly bool[] _blockAccessList = new bool[0x1000];
         //private const int CHUNKS_NUM = 5;
         //private const int MAX_CHUNKS = CHUNKS_NUM * 2 + 1;
@@ -60,12 +60,24 @@ namespace ClassicUO.Game.Map
                 if (_center != value)
                 {
                     _center = value;
-                    LoadChunks((ushort) _center.X, (ushort) _center.Y);
+                    LoadChunks((ushort)_center.X, (ushort)_center.Y);
                 }
             }
         }
 
-        public  Tile GetTile(short x, short y, bool load = true)
+        public MapChunk GetMapChunk(int rblock, int blockX, int blockY)
+        {
+            ref MapChunk chunk = ref Chunks[rblock];
+            if (chunk == null)
+            {
+                _usedIndices.Add(rblock);
+                chunk = new MapChunk((ushort)blockX, (ushort)blockY);
+                chunk.Load(Index);
+            }
+            return chunk;
+        }
+
+        public Tile GetTile(short x, short y, bool load = true)
         {
             if (x < 0 || y < 0) return null;
             int cellX = x / 8;
@@ -81,7 +93,7 @@ namespace ClassicUO.Game.Map
                 if (load)
                 {
                     _usedIndices.Add(block);
-                    chuck = new MapChunk((ushort) cellX, (ushort) cellY);
+                    chuck = new MapChunk((ushort)cellX, (ushort)cellY);
                     chuck.Load(Index);
                 }
                 else
@@ -93,9 +105,9 @@ namespace ClassicUO.Game.Map
             return chuck.Tiles[x % 8][y % 8];
         }
 
-        public  Tile GetTile(int x, int y, bool load = true)
+        public Tile GetTile(int x, int y, bool load = true)
         {
-            return  GetTile((short) x, (short) y, load);
+            return GetTile((short)x, (short)y, load);
         }
 
         public sbyte GetTileZ(int x, int y)
@@ -111,8 +123,8 @@ namespace ClassicUO.Game.Map
 
             unsafe
             {
-                MapBlock* mp = (MapBlock*) blockIndex.MapAddress;
-                MapCells* cells = (MapCells*) &mp->Cells;
+                MapBlock* mp = (MapBlock*)blockIndex.MapAddress;
+                MapCells* cells = (MapCells*)&mp->Cells;
 
                 return cells[my * 8 + mx].Z;
             }
@@ -124,7 +136,7 @@ namespace ClassicUO.Game.Map
             {
                 fixed (bool* ptr = _blockAccessList)
                 {
-                    byte* start = (byte*) ptr;
+                    byte* start = (byte*)ptr;
                     byte* end = start + _blockAccessList.Length;
                     while (&start[0] != &end[0]) *start++ = 0;
                 }
@@ -138,7 +150,7 @@ namespace ClassicUO.Game.Map
             if (access)
                 return defaultZ;
             access = true;
-             Tile tile =  GetTile(x, y);
+            Tile tile = GetTile(x, y);
 
             if (tile != null)
             {
@@ -260,7 +272,7 @@ namespace ClassicUO.Game.Map
                         if (CoreGame.Ticks - tick >= maxDelay)
                             return;
                         _usedIndices.Add(cellindex);
-                        chunk = new MapChunk((ushort) i, (ushort) j);
+                        chunk = new MapChunk((ushort)i, (ushort)j);
                         chunk.Load(Index);
                     }
 
