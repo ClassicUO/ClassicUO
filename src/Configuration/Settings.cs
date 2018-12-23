07492c42e4b420565d1d1771930f9866d9723b19
Andrea Angelini <andrea-angelini@outlook.it>
2018-12-23 17:44:53 +0100

    + added autologin setting prop + removed old class

--- src/Configuration/Settings.cs
+++ src/Configuration/Settings.cs
@@ -29,142 +29,40 @@ using Newtonsoft.Json;
 
 namespace ClassicUO.Configuration
 {
-    public sealed class Settings : NotifyPropertyChange
+    public sealed class Settings
     {
-        private string _clientVersion = "7.0.59.8";
-        private bool _debug = true;
-        private string _ip = "YOUR.SERVER.IP.ADDRESS";
-        private string _lastCharName = "";
-        private int _maxFPS = 144;
-        private string _password = ""; //important default otherwise TextBox.SetText crashes from null input on Main menu
-        private ushort _port = 2593;
-        private bool _preloadMaps;
-        private bool _profiler = true;
-        private string _uoDir = "YOUR\\PATH\\TO\\ULTIMAONLINE";
-        private string _username = ""; //important default otherwise TextBox.SetText crashes from null input on Main menu
-
-
         [JsonConstructor]
         public Settings()
         {
         }
 
-        [JsonProperty(PropertyName = "username")]
-        public string Username
-        {
-            get => _username;
-            set => SetProperty(ref _username, value);
-        }
+        [JsonProperty(PropertyName = "username")] public string Username { get; set; } = string.Empty;
 
-        [JsonProperty(PropertyName = "password")]
-        public string Password
-        {
-            get => _password;
-            set => SetProperty(ref _password, value);
-        }
+        [JsonProperty(PropertyName = "password")] public string Password { get; set; } = string.Empty;
 
-        [JsonProperty(PropertyName = "ip")]
-        public string IP
-        {
-            get => _ip;
-            set => SetProperty(ref _ip, value);
-        }
+        [JsonProperty(PropertyName = "ip")] public string IP { get; set; } = "127.0.0.1";
 
-        [JsonProperty(PropertyName = "port")]
-        public ushort Port
-        {
-            get => _port;
-            set => SetProperty(ref _port, value);
-        }
+        [JsonProperty(PropertyName = "port")] public ushort Port { get; set; } = 2593;
 
-        [JsonProperty(PropertyName = "lastcharactername")]
-        public string LastCharacterName
-        {
-            get => _lastCharName;
-            set => SetProperty(ref _lastCharName, value);
-        }
+        [JsonProperty(PropertyName = "lastcharactername")] public string LastCharacterName { get; set; } = string.Empty;
 
-        [JsonProperty(PropertyName = "ultimaonlinedirectory")]
-        public string UltimaOnlineDirectory
-        {
-            get => _uoDir;
-            set => SetProperty(ref _uoDir, value);
-        }
+        [JsonProperty(PropertyName = "ultimaonlinedirectory")] public string UltimaOnlineDirectory { get; set; } = string.Empty;
 
-        [JsonProperty(PropertyName = "clientversion")]
-        public string ClientVersion
-        {
-            get => _clientVersion;
-            set => SetProperty(ref _clientVersion, value);
-        }
+        [JsonProperty(PropertyName = "clientversion")] public string ClientVersion { get; set; } = "7.0.59.8";
 
-        [JsonProperty(PropertyName = "maxfps")]
-        public int MaxFPS
-        {
-            get => _maxFPS;
-            set => SetProperty(ref _maxFPS, value);
-        }
+        [JsonProperty(PropertyName = "maxfps")]public int MaxFPS { get; set; } = 144;
 
-        [JsonProperty(PropertyName = "debug")]
-        public bool Debug
-        {
-            get => _debug;
-            set => SetProperty(ref _debug, value);
-        }
+        [JsonProperty(PropertyName = "debug")] public bool Debug { get; set; } = true;
 
-        [JsonProperty(PropertyName = "profiler")]
-        public bool Profiler
-        {
-            get => _profiler;
-            set => SetProperty(ref _profiler, value);
-        }
+        [JsonProperty(PropertyName = "profiler")] public bool Profiler { get; set; } = true;
 
-        [JsonProperty(PropertyName = "preload_maps")]
-        public bool PreloadMaps
-        {
-            get => _preloadMaps;
-            set => SetProperty(ref _preloadMaps, value);
-        }
+        [JsonProperty(PropertyName = "preload_maps")] public bool PreloadMaps { get; set; }
 
-
+        [JsonProperty(PropertyName = "autologin")] public bool AutoLogin { get; set; }
 
         public void Save()
         {
             ConfigurationResolver.Save(this, "settings.json");
-        }
-    }
-
-    public abstract class NotifyPropertyChange
-    {
-        [JsonIgnore] public EventHandler PropertyChanged;
-
-        public virtual bool SetProperty<T>(ref T storage, T value)
-        {
-            if (EqualityHelper.IsEqual(storage, value))
-                return false;
-            storage = value;
-            OnPropertyChanged();
-
-            return true;
-        }
-
-        protected virtual void OnPropertyChanged()
-        {
-            PropertyChanged.Raise();
-        }
-    }
-
-    public static class EqualityHelper
-    {
-        public static bool IsEqual<T>(T oldValue, T newValue)
-        {
-            if (oldValue == null && newValue == null) return true;
-            if (oldValue == null || newValue == null) return false;
-            Type type = typeof(T);
-
-            if (type.IsValueType) return oldValue.Equals(newValue);
-
-            return Equals(oldValue, newValue);
         }
     }
 }
