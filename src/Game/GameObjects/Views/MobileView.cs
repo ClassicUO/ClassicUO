2027fc9c57c70ec9624f9517d11c6118d2b66369
Andrea Angelini <andrea-angelini@outlook.it>
2019-01-27 12:02:27 +0100

    + possible fix mini healthbar

--- src/Game/GameObjects/Views/MobileView.cs
+++ src/Game/GameObjects/Views/MobileView.cs
@@ -180,11 +180,11 @@ namespace ClassicUO.Game.GameObjects
 
             if (needHpLine)
             {
-                position.X += Engine.Profile.Current.GameWindowPosition.X + 9;
-                position.Y += Engine.Profile.Current.GameWindowPosition.Y + 30;
+                //position.X += Engine.Profile.Current.GameWindowPosition.X + 9;
+                //position.Y += Engine.Profile.Current.GameWindowPosition.Y + 30;
 
-                TargetLineGump.TTargetLineGump.X = (int)(position.X /*+ 22*/ + Offset.X);
-                TargetLineGump.TTargetLineGump.Y = (int)(position.Y /*+ 22 + (mobile.IsMounted ? 22 : 0) */+ Offset.Y - Offset.Z - 3);
+                //TargetLineGump.TTargetLineGump.X = (int)(position.X /*+ 22*/ + Offset.X);
+                //TargetLineGump.TTargetLineGump.Y = (int)(position.Y /*+ 22 + (mobile.IsMounted ? 22 : 0) */+ Offset.Y - Offset.Z - 3);
                 TargetLineGump.TTargetLineGump.BackgroudHue = targetColor;
                 
                 if (IsPoisoned)
