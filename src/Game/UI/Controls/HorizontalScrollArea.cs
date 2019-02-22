e8ef200b6f70ad18eb24815f5c4b303e0ba7b4a9
Andrea Angelini <andrea-angelini@outlook.it>
2019-02-22 23:26:53 +0100

    + menu gump

---
+++ src/Game/UI/Controls/HorizontalScrollArea.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClassicUO.Renderer;
+
+using Microsoft.Xna.Framework;
+
+namespace ClassicUO.Game.UI.Controls
+{
+    //class HorizontalScrollArea : Control
+    //{
+    //    private Rectangle _rect;
+
+    //    public HorizontalScrollArea(int x, int y, int w, int h)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //        Width = w;
+    //        Height = h;
+
+    //        AcceptMouseInput = true;
+    //        WantUpdateSize = false;
+    //        CanMove = true;
+
+
+    //        HSliderBar bar = new HSliderBar(0, 0, w, 0, 100, 0, HSliderBarStyle.BlueWidgetNoBar)
+    //        {
+    //            Parent = this
+    //        };
+    //    }
+
+
+    //    public override void Update(double totalMS, double frameMS)
+    //    {
+    //        base.Update(totalMS, frameMS);
+    //    }
+
+    //    public override bool Draw(Batcher2D batcher, Point position, Vector3? hue = null)
+    //    {
+    //        Children[0].Draw(batcher, new Point(position.X + Children[0].X, position.Y + Children[0].Y));
+    //        _rect.X = position.X;
+    //        _rect.Y = position.Y;
+    //        _rect.Width = Width;
+    //        _rect.Height = Height;
+
+    //        Rectangle scissor = ScissorStack.CalculateScissors(batcher.TransformMatrix, _rect);
+
+    //        if (ScissorStack.PushScissors(scissor))
+    //        {
+    //            batcher.EnableScissorTest(true);
+
+    //            int width = 0;
+    //            int maxWidth = 
+
+
+    //            batcher.EnableScissorTest(false);
+    //            ScissorStack.PopScissors();
+    //        }
+
+    //        return true;
+    //    }
+    //}
+}
--- src/Game/UI/Gumps/MenuGump.cs
+++ src/Game/UI/Gumps/MenuGump.cs
@@ -5,13 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 using ClassicUO.Game.UI.Controls;
+using ClassicUO.Input;
 using ClassicUO.Renderer;
+
+using Microsoft.Xna.Framework;
 
 namespace ClassicUO.Game.UI.Gumps
 {
     class MenuGump : Gump
     {
-        private readonly ScrollArea _scrollArea;
+        private readonly ContainerHorizontal _container;
+        private bool _isDown, _isLeft;
 
         public MenuGump(Serial serial, string name) : base(serial, 0)
         {
@@ -33,10 +37,66 @@ namespace ClassicUO.Game.UI.Gumps
 
             Add(label);
 
-            _scrollArea = new ScrollArea(40, 42, 217, 49, true);
-            
-            Add(_scrollArea);
+            _container = new ContainerHorizontal()
+            {
+                X = 40,
+                Y = 42,
+                Width = 217,
+                Height = 49,
+                WantUpdateSize = false
+            };
+
+            Add(_container);
+
+
+
+
+            HitBox left = new HitBox(25, 60, 10, 15)
+            {
+                IsTransparent = false,
+                Alpha = 1,
+            };
+            left.MouseDown += (sender, e) =>
+            {
+                _isDown = true;
+                _isLeft = true;
+            };
+
+            left.MouseUp += (sender, e) =>
+            {
+                _isDown = false;
+            };
+            Add(left);
+
+
+            HitBox right = new HitBox(260, 60, 10, 15)
+            {
+                IsTransparent = false,
+                Alpha = 1,
+            };
+            right.MouseDown += (sender, e) =>
+            {
+                _isDown = true;
+                _isLeft = false;
+            };
+
+            right.MouseUp += (sender, e) =>
+            {
+                _isDown = false;
+            };
+            Add(right);
         }
+
+        public override void Update(double totalMS, double frameMS)
+        {
+            base.Update(totalMS, frameMS);
+
+            if (_isDown)
+            {
+                _container.Value += (_isLeft ? -1 : 1);
+            }
+        }
+
 
         public void AddItem(Graphic graphic, Hue hue, string name, int x, int y)
         {
@@ -48,7 +108,124 @@ namespace ClassicUO.Game.UI.Gumps
             };
             pic.SetTooltip(name);
 
-            _scrollArea.Add(pic);
+
+            _container.Add(pic);
+        }
+
+        class ContainerHorizontal : Control
+        {
+            private Rectangle _rect;
+            private int _maxWidth;
+            private bool _update = true;
+            private int _value;
+
+            public ContainerHorizontal()
+            {
+
+            }
+
+            public int Value
+            {
+                get => _value;
+                set
+                {
+                    if (value < 0)
+                        value = 0;
+                    else if (value > _maxWidth)
+                        value = _maxWidth;
+
+                    _value = value;
+                }
+            }
+
+            protected override void OnInitialize()
+            {
+                _update = true;
+                base.OnInitialize();
+            }
+
+            public override void Update(double totalMS, double frameMS)
+            {
+                base.Update(totalMS, frameMS);
+
+                if (_update)
+                {
+                    _update = false;
+
+                    CalculateWidth();
+                }
+            }
+
+            public override bool Draw(Batcher2D batcher, Point position, Vector3? hue = null)
+            {
+                _rect.X = position.X;
+                _rect.Y = position.Y;
+                _rect.Width = Width;
+                _rect.Height = Height;
+
+                Rectangle scissor = ScissorStack.CalculateScissors(batcher.TransformMatrix, _rect);
+
+                if (ScissorStack.PushScissors(scissor))
+                {
+                    batcher.EnableScissorTest(true);
+
+                    int width = 0;
+                    int maxWidth = Value + Width;
+                    bool drawOnly1 = true;
+
+                    position = _rect.Location;
+
+                    for (int i = 0; i < Children.Count; i++)
+                    {
+                        Control child = Children[i];
+
+                        if (!child.IsVisible)
+                            continue;
+
+                        child.X = width - Value;
+
+                        if (width + child.Width <= Value)
+                        {
+
+                        }
+                        else if (width + child.Width <= maxWidth)
+                        {
+                            child.Draw(batcher, new Point(position.X + child.X, position.Y + child.Y));
+                        }
+                        else
+                        {
+                            if (drawOnly1)
+                            {
+                                child.Draw(batcher, new Point(position.X + child.X, position.Y + child.Y));
+                                drawOnly1 = false;
+                            }
+                        }
+
+                        width += child.Width;
+                    }
+
+
+                    batcher.EnableScissorTest(false);
+                    ScissorStack.PopScissors();
+                }
+
+                return true; // base.Draw(batcher,position, hue);
+            }
+
+            protected override void OnChildAdded()
+            {
+                _update = true;
+            }
+
+            protected override void OnChildRemoved()
+            {
+                _update = true;
+            }
+
+            private void CalculateWidth()
+            {
+                _maxWidth = Children.Sum(s => s.Width) - Width;
+            }
         }
 
 
