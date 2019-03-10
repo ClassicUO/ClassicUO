a48fe7263a4bd21dd28be176a18a62ad2a0794fc
Andrea Angelini <andrea-angelini@outlook.it>
2019-03-10 15:22:31 +0100

    re enabled vertex check

--- src/Renderer/Batcher2D.cs
+++ src/Renderer/Batcher2D.cs
@@ -141,20 +141,20 @@ namespace ClassicUO.Renderer
         {
             if (texture == null || texture.IsDisposed)
                 return false;
-            //bool draw = false;
 
-            //for (byte i = 0; i < 4; i++)
-            //{
-            //    if (_drawingArea.Contains(vertices[i].Position) == ContainmentType.Contains)
-            //    {
-            //        draw = true;
+            bool draw = false;
 
-            //        break;
-            //    }
-            //}
+            for (byte i = 0; i < 4; i++)
+            {
+                if (_drawingArea.Contains(vertices[i].Position) == ContainmentType.Contains)
+                {
+                    draw = true;
+                    break;
+                }
+            }
 
-            //if (!draw)
-            //    return false;
+            if (!draw)
+                return false;
 
             if (_numSprites >= MAX_SPRITES)
                 Flush();
