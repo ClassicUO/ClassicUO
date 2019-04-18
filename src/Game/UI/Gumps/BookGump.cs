4e488dd62737616ccf4ff7b464a379cd915a96ce
fwiffo <fwiffo@fwiffo.net>
2019-04-18 17:45:28 +0200

    if non editable, the bookgump gives the page based on the mouse selection

--- src/Game/UI/Gumps/BookGump.cs
+++ src/Game/UI/Gumps/BookGump.cs
@@ -131,7 +131,7 @@ namespace ClassicUO.Game.UI.Gumps
 
         private readonly List<MultiLineBox> m_Pages = new List<MultiLineBox> ();
         private int MaxPage => (BookPageCount >> 1) + 1;
-        private int ActiveInternalPage => Math.Max(0, m_Pages.FindIndex(t => t.HasKeyboardFocus));
+        private int ActiveInternalPage => IsEditable ? m_Pages.FindIndex(t => t.HasKeyboardFocus) : m_Pages.FindIndex(t => t.MouseIsOver);
 
         private void SetActivePage( int page )
         {
