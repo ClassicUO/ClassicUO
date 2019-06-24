cc0a81b069837d96c67367b03b0b7f54de1fd1e4
fwiffo <fwiffo@fwiffo.net>
2019-06-24 18:57:35 +0200

    old StatusGump doesn't have any locks, so avoid setting them as it would throw an exception on load

--- src/Game/UI/Gumps/StatusGump.cs
+++ src/Game/UI/Gumps/StatusGump.cs
@@ -196,17 +196,20 @@ namespace ClassicUO.Game.UI.Gumps
 
         public void UpdateLocksAfterPacket()
         {
-            for (int i = 0; i < 3; i++)
+            if (Engine.GlobalSettings.ShardType != 1)
             {
-                Lock status = i == 0 ? World.Player.StrLock : i == 1 ? World.Player.DexLock : World.Player.IntLock;
+                for (int i = 0; i < 3; i++)
+                {
+                    Lock status = i == 0 ? World.Player.StrLock : i == 1 ? World.Player.DexLock : World.Player.IntLock;
 
-                ushort gumpID = 0x0984; //Up
+                    ushort gumpID = 0x0984; //Up
 
-                if (status == Lock.Down)
-                    gumpID = 0x0986; //Down
-                else if (status == Lock.Locked)
-                    gumpID = 0x082C; //Lock
-                _lockers[i].Graphic = gumpID;
+                    if (status == Lock.Down)
+                        gumpID = 0x0986; //Down
+                    else if (status == Lock.Locked)
+                        gumpID = 0x082C; //Lock
+                    _lockers[i].Graphic = gumpID;
+                }
             }
         }
 
