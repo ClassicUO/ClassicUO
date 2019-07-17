674d55805157a51fedae259ebcd77a4de2fe5a77
Andrea Angelini <andrea-angelini@outlook.it>
2019-07-17 17:00:04 +0200

    show container locked overhread

--- src/Game/Scenes/GameSceneDrawingSorting.cs
+++ src/Game/Scenes/GameSceneDrawingSorting.cs
@@ -266,7 +266,7 @@ namespace ClassicUO.Game.Scenes
                 {
                     obj.UseObjectHandles = (ismobile ||
                                             iscorpse ||
-                                            obj is Item it && (!it.IsLocked/* || it.IsLocked && itemData.IsContainer*/) && !it.IsMulti) &&
+                                            obj is Item it && (!it.IsLocked || it.IsLocked && itemData.IsContainer) && !it.IsMulti) &&
                                            !obj.ClosedObjectHandles && _objectHandlesCount <= 400;
                     _objectHandlesCount++;
                 }
