5f2dc4b17fdd166d9ec1795a58edda553ecdae3e
Andrea Angelini <andrea-angelini@outlook.it>
2019-08-04 20:21:25 +0200

    + rendered text hue selection fix

--- src/Renderer/RenderedText.cs
+++ src/Renderer/RenderedText.cs
@@ -224,8 +224,10 @@ namespace ClassicUO.Renderer
             {
                 if (IsUnicode)
                     _hueVector.Y = ShaderHuesTraslator.SHADER_TEXT_HUE_NO_BALCK;
+                else if (Font == 3)
+                    _hueVector.Y = 5;
                 else if (Font != 5 && Font != 8)
-                    _hueVector.Y = ShaderHuesTraslator.SHADER_TEXT_HUE_NO_BALCK;
+                    _hueVector.Y = ShaderHuesTraslator.SHADER_PARTIAL_HUED;
                 else
                     _hueVector.Y = ShaderHuesTraslator.SHADER_HUED;
             }
