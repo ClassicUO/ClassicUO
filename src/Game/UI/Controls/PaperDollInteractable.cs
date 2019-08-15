2e79c156f1ad51dd5a50a5b7754db05e4d2d70c9
Andrea Angelini <andrea-angelini@outlook.it>
2019-08-15 10:43:45 +0200

    + hair and beard unpickable

--- src/Game/UI/Controls/PaperDollInteractable.cs
+++ src/Game/UI/Controls/PaperDollInteractable.cs
@@ -263,8 +263,12 @@ namespace ClassicUO.Game.UI.Controls
 
                         Item item = _mobile.Equipment[layerIndex];
                         bool isfake = false;
-                        bool canPickUp = World.InGame && (World.Player.Graphic == 0x03DB || World.Player == Mobile || (World.Player.Flags & Flags.IgnoreMobiles) != 0 || 
-                            (Mobile.NotorietyFlag == NotorietyFlag.Invulnerable && (Mobile.Flags & Flags.IgnoreMobiles) == 0));
+                        bool canPickUp = World.InGame && (World.Player.Graphic == 0x03DB || 
+                                                          World.Player == Mobile || 
+                                                          (World.Player.Flags & Flags.IgnoreMobiles) != 0 || 
+                                                          (Mobile.NotorietyFlag == NotorietyFlag.Invulnerable && (Mobile.Flags & Flags.IgnoreMobiles) == 0)) && 
+                                         layerIndex != (int) Layer.Hair && 
+                                         layerIndex != (int) Layer.Beard;
                         ref var itemGump = ref _pgumps[layerIndex];
 
                         if (_fakeItem != null && _fakeItem.ItemData.Layer == layerIndex)
