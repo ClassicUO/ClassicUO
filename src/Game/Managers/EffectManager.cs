d52641dfe9bb88ff502245e8314638871c88a71a
Andrea Angelini <andrea-angelini@outlook.it>
2019-11-29 23:29:34 +0100

    remove update out the loop

--- src/Game/Managers/EffectManager.cs
+++ src/Game/Managers/EffectManager.cs
@@ -81,7 +81,6 @@ namespace ClassicUO.Game.Managers
                     if (doesExplode)
                         effect.AddChildEffect(new AnimatedItemEffect(target, targPos.X, targPos.Y, targPos.Z, 0x36Cb, hue, 9, speed));
 
-                    effect.Update(Time.Ticks, 0);
                     break;
 
                 case GraphicEffectType.Lightning:
