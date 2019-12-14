19a2c962b84ea08d4bb15a2ab5c339fea8ea95ef
Andrea Angelini <andrea-angelini@outlook.it>
2019-12-14 14:40:27 +0100

    endlessjourney login background

--- src/Game/UI/Gumps/Login/LoginBackground.cs
+++ src/Game/UI/Gumps/Login/LoginBackground.cs
@@ -22,6 +22,7 @@
 #endregion
 
 using ClassicUO.Game.UI.Controls;
+using ClassicUO.IO;
 
 namespace ClassicUO.Game.UI.Gumps.Login
 {
@@ -29,23 +30,32 @@ namespace ClassicUO.Game.UI.Gumps.Login
     {
         public LoginBackground() : base(0, 0)
         {
-            // Background
-            Add(new GumpPicTiled(0, 0, 640, 480, 0x0E14) {AcceptKeyboardInput = false});
-            // Border
-            Add(new GumpPic(0, 0, 0x157C, 0) {AcceptKeyboardInput = false});
-
-            // UO Flag
-            Add(new GumpPic(0, 4, 0x15A0, 0) {AcceptKeyboardInput = false});
-
-
-            // Quit Button
-            Add(new Button(0, 0x1589, 0x158B, 0x158A)
+            if (UOFileManager.ClientVersion >= ClientVersions.CV_706400)
             {
-                X = 555,
-                Y = 4,
-                ButtonAction = ButtonAction.Activate,
-                AcceptKeyboardInput = false
-            });
+                // Background
+                Add(new GumpPicTiled(0, 0, 640, 480, 0x0150) { AcceptKeyboardInput = false });
+                // UO Flag
+                Add(new GumpPic(0, 4, 0x0151, 0) { AcceptKeyboardInput = false });
+            }
+            else
+            {
+                // Background
+                Add(new GumpPicTiled(0, 0, 640, 480, 0x0E14) { AcceptKeyboardInput = false });
+                // Border
+                Add(new GumpPic(0, 0, 0x157C, 0) { AcceptKeyboardInput = false });
+                // UO Flag
+                Add(new GumpPic(0, 4, 0x15A0, 0) { AcceptKeyboardInput = false });
+
+                // Quit Button
+                Add(new Button(0, 0x1589, 0x158B, 0x158A)
+                {
+                    X = 555,
+                    Y = 4,
+                    ButtonAction = ButtonAction.Activate,
+                    AcceptKeyboardInput = false
+                });
+            }
+
 
             CanCloseWithEsc = false;
             CanCloseWithRightClick = false;
