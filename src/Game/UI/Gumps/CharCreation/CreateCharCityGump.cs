31407393e5c2070e2a00b07025a526e8b9cb973c
Andrea Angelini <andrea-angelini@outlook.it>
2019-12-28 20:50:40 +0100

    fix invisible cities

--- src/Game/UI/Gumps/CharCreation/CreateCharCityGump.cs
+++ src/Game/UI/Gumps/CharCreation/CreateCharCityGump.cs
@@ -126,7 +126,7 @@ namespace ClassicUO.Game.UI.Gumps.CharCreation
                 _facetName.IsVisible = false;
             }
 
-            if (Settings.GlobalSettings.ShardType != 2)
+            if (Settings.GlobalSettings.ShardType == 2)
                 _facetName.IsVisible = false;
 
             Add(_facetName);
@@ -150,7 +150,7 @@ namespace ClassicUO.Game.UI.Gumps.CharCreation
             _htmlControl = new HtmlControl(452, 60, 175, 367, true, true, ishtml: true, text: city.Description);
             Add(_htmlControl);
 
-            if (Settings.GlobalSettings.ShardType != 2)
+            if (Settings.GlobalSettings.ShardType == 2)
                 _htmlControl.IsVisible = false;
 
             for (int i = 0; i < scene.Cities.Length; i++)
@@ -183,7 +183,7 @@ namespace ClassicUO.Game.UI.Gumps.CharCreation
                 Add(control);
                 _cityControls.Add(control);
 
-                if (Settings.GlobalSettings.ShardType != 2)
+                if (Settings.GlobalSettings.ShardType == 2)
                     control.IsVisible = false;
             }
 
