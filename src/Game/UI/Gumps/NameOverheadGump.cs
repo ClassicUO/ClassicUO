5a54811f5394afaf1bba914fde58570497e65d21
Andrea Angelini <andrea-angelini@outlook.it>
2019-12-28 17:22:56 +0100

    fix mouseover in nameplates when cursor is on and when you are flying

--- src/Game/UI/Gumps/NameOverheadGump.cs
+++ src/Game/UI/Gumps/NameOverheadGump.cs
@@ -321,7 +321,7 @@ namespace ClassicUO.Game.UI.Gumps
                                                               out int height);
 
                 _lockedPosition.X = (int)((Entity.RealScreenPosition.X + m.Offset.X + 22) / scale);
-                _lockedPosition.Y = (int)((Entity.RealScreenPosition.Y + (m.Offset.Y - m.Offset.Z) - (height + centerY + 8) + (!m.IsMounted ? 22 : 0)) / scale);
+                _lockedPosition.Y = (int)((Entity.RealScreenPosition.Y + (m.Offset.Y - m.Offset.Z) - (height + centerY + 8) + (m.IsFlying ? -22 : !m.IsMounted ? 22 : 0)) / scale);
             }
 
             base.OnMouseOver(x, y);
@@ -398,7 +398,7 @@ namespace ClassicUO.Game.UI.Gumps
                                                                   out int height);
 
                     x = (int)((Entity.RealScreenPosition.X + m.Offset.X + 22) / scale);
-                    y = (int)((Entity.RealScreenPosition.Y + (m.Offset.Y - m.Offset.Z) - (height + centerY + 8) + ( m.IsFlying ? -22 : !m.IsMounted ? 22 : 0)) / scale);
+                    y = (int)((Entity.RealScreenPosition.Y + (m.Offset.Y - m.Offset.Z) - (height + centerY + 8) + (m.IsFlying ? -22 : !m.IsMounted ? 22 : 0)) / scale);
                 }
             }
             else if (Entity.Texture != null)
