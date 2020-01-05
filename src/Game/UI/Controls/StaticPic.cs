c3608998e6dd48cd1be37d81fabc112fa1d03f82
Andrea Angelini <andrea-angelini@outlook.it>
2020-01-05 23:33:03 +0100

    + fixed some visual issue on housecustomization gump

--- src/Game/UI/Controls/StaticPic.cs
+++ src/Game/UI/Controls/StaticPic.cs
@@ -67,7 +67,7 @@ namespace ClassicUO.Game.UI.Controls
             ResetHueVector();
             ShaderHuesTraslator.GetHueVector(ref _hueVector, Hue, _isPartial, 0);
 
-            batcher.Draw2D(Texture, x, y, ref _hueVector);
+            batcher.Draw2D(Texture, x, y, /*Width, Height,*/ ref _hueVector);
 
             return base.Draw(batcher, x, y);
         }
