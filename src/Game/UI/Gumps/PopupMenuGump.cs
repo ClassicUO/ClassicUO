3f036168241db52cb90fff43e434a435927be302
Andrea Angelini <andrea-angelini@outlook.it>
2020-01-21 11:09:12 +0100

    minors

--- src/Game/UI/Gumps/PopupMenuGump.cs
+++ src/Game/UI/Gumps/PopupMenuGump.cs
@@ -35,8 +35,6 @@ namespace ClassicUO.Game.UI.Gumps
         public PopupMenuGump(PopupMenuData data) : base(0, 0)
         {
             CloseIfClickOutside = true;
-            //ControlInfo.IsModal = true;
-            //ControlInfo.ModalClickOutsideAreaClosesThisControl = true;
             CanMove = false;
             CanCloseWithRightClick = true;
             ResizePic pic = new ResizePic(0x0A3C)
