e6381036aa66f7df9085b83592150ee5f9dc554a
Andrea Angelini <andrea-angelini@outlook.it>
2020-01-28 11:02:04 +0100

    + moving enum

--- src/Client.cs
+++ src/Client.cs
@@ -41,23 +41,6 @@ using SDL2;
 
 namespace ClassicUO
 {
-    [Flags]
-    enum ClientFlags : uint
-    {
-        CF_T2A = 0x00,
-        CF_RE = 0x01,
-        CF_TD = 0x02,
-        CF_LBR = 0x04,
-        CF_AOS = 0x08,
-        CF_SE = 0x10,
-        CF_SA = 0x20,
-        CF_UO3D = 0x40,
-        CF_RESERVED = 0x80,
-        CF_3D = 0x100,
-        CF_UNDEFINED = 0xFFFF,
-    }
-
-
     static class Client
     {
         public static ClientVersion Version { get; private set; } 
