85062f9ebc43aa4b15d2560620fa575c467e89d5
Andrea Angelini <andrea-angelini@outlook.it>
2020-03-25 18:54:05 +0100

    vers

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.3.200")]
-[assembly: AssemblyFileVersion("0.1.3.200")]
+[assembly: AssemblyVersion("0.1.3.300")]
+[assembly: AssemblyFileVersion("0.1.3.300")]
