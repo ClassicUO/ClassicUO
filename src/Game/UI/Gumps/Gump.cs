aae6b81d215eb357e864aff77ebf3a7f6d9b7c8e
Andrea Angelini <andrea-angelini@outlook.it>
2020-06-12 17:03:20 +0200

    dont create rectangle

--- src/Game/UI/Gumps/Gump.cs
+++ src/Game/UI/Gumps/Gump.cs
@@ -131,10 +131,11 @@ namespace ClassicUO.Game.UI.Gumps
 
         public void SetInScreen()
         {
-            Rectangle rect = new Rectangle(0, 0, Client.Game.Window.ClientBounds.Width, Client.Game.Window.ClientBounds.Height);
-
-            if (rect.Intersects(Bounds))
+            if (Bounds.Width >= 0 && Bounds.X <= Client.Game.Window.ClientBounds.Width &&
+                Bounds.Height >= 0 && Bounds.Y <= Client.Game.Window.ClientBounds.Height)
+            {
                 return;
+            }
 
             X = 0;
             Y = 0;
