92c1d291a02f8ab46c7e590ef7c053c9f88efcbb
Andrea Angelini <andrea-angelini@outlook.it>
2020-08-01 17:09:57 +0200

    missed one file to commit :D

--- src/Utility/GameObjectHelper.cs
+++ src/Utility/GameObjectHelper.cs
@@ -20,7 +20,7 @@
 #endregion
 
 using System.Runtime.CompilerServices;
-
+using ClassicUO.Data;
 using ClassicUO.Game;
 using ClassicUO.Game.Data;
 using ClassicUO.IO.Resources;
@@ -32,17 +32,11 @@ namespace ClassicUO.Utility
         [MethodImpl(256)]
         public static bool IsNoDrawable(ushort g)
         {
-            if (g == 0xf65)
-            {
-
-            }
-
             switch (g)
             {
                 case 0x0001:
                 case 0x21BC:
                 //case 0x5690:
-                case 0x0F65: // easel fix
                     return true;
 
                 case 0x9E4C:
@@ -58,6 +52,13 @@ namespace ClassicUO.Utility
             {
                 if (g >= 0x2198 && g <= 0x21A4) 
                     return true;
+
+                // Easel fix.
+                // In older clients the tiledata flag for this 
+                // item contains NoDiagonal for some reason.
+                // So the next check will make the item invisible.
+                if (g == 0x0F65 && Client.Version < ClientVersion.CV_60144)
+                    return false;
 
                 ref StaticTiles data = ref TileDataLoader.Instance.StaticData[g];
                 if (!data.IsNoDiagonal || (data.IsAnimated && World.Player != null && World.Player.Race == RaceType.GARGOYLE))
