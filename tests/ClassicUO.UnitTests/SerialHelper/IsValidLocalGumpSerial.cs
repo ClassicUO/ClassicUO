e725c8b692222cff947ee206fb67830a9991e974
deccer <decccer@outlook.com>
2020-08-25 09:52:40 +0400

    Add SerialHelper unit tests

---
+++ tests/ClassicUO.UnitTests/SerialHelper/IsValidLocalGumpSerial.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ClassicUO.UnitTests.SerialHelper
+{
+    public class IsValidLocalGumpSerial
+    {
+        [Theory]
+        [InlineData(Game.Constants.JOURNAL_LOCALSERIAL)]
+        [InlineData(0xFFFFFFFE)]
+        public void IsValidLocalGumpSerial_Should_Not_Legal(uint serial)
+        {
+            Game.SerialHelper.IsValidLocalGumpSerial(serial)
+                .Should()
+                .BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(0xFFFFFFE0)]
+        [InlineData(0xFFFFFFFF)]
+        public void IsValidLocalGumpSerial_Should_Not_BeLegal(uint serial)
+        {
+            Game.SerialHelper.IsValidLocalGumpSerial(serial)
+                .Should()
+                .BeFalse();
+        }
+    }
+}
