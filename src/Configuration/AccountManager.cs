0f00b0202f6389882f2e8aab8e4621e40854e4ce
Kyle MacDonald <kmacdonald1983@gmail.com>
2020-08-29 16:02:39 -0500

    AccountManager cleanup.

--- src/Configuration/AccountManager.cs
+++ src/Configuration/AccountManager.cs
@@ -1,6 +1,8 @@
 ﻿using ClassicUO.Utility;
+using ClassicUO.Utility.Logging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 
@@ -8,77 +10,88 @@ namespace ClassicUO.Configuration
 {
     internal static class AccountManager
     {
-        public static List<Account> Accounts;
+        private static List<Account> _accounts;
 
-        public static string[] GetAccountNames(string serverName)
+        public static IReadOnlyCollection<Account> GetAccountNames(string serverName)
         {
-            try
-            {
-                Load(serverName);
-                return Accounts?.Where(x => x.Server == serverName).Select(x => x.UserName).OrderBy(x => x).ToArray() ?? new string[] { };
-            }
-            catch(Exception ex)
-            {
-                //Log error?
-                return new string[] { };
-            }
+            LoadAccounts(serverName);
+            var accounts = _accounts?
+                .Where(account => account.Server == serverName)
+                .OrderBy(account => account.UserName)
+                .ToList() ?? new List<Account>();
+
+            return new ReadOnlyCollection<Account>(accounts);
         }
 
         public static string GetAccountPassword(string serverName, string userName)
         {
-            try
-            {
-                Load(serverName);
-                return Accounts?.FirstOrDefault(x => x.UserName == userName)?.Password;
-            }
-            catch(Exception ex)
-            {
-                //Log Error?
-                return string.Empty;
-            }
+            LoadAccounts(serverName);
+            return _accounts?.FirstOrDefault(account => account.UserName == userName)?.Password ?? string.Empty;
         }
 
         public static void SaveAccount(string serverName, string userName, string password, bool saveAccount)
         {
+            LoadAccounts(serverName);
+            var existingRecord = _accounts.FirstOrDefault(x => x.Server == serverName && x.UserName == userName);
+            //There is an existing record and the user has opted to not save that account, remove from list.
+            if (existingRecord != null && !saveAccount)
+            {
+                _accounts.Remove(existingRecord);
+            }
+            //There is an existing record and they have updated their password.  save the udpated password.
+            else if (existingRecord != null && existingRecord.Password != password)
+            {
+                existingRecord.Password = password;
+            }
+            //Otherwise if there is no existing record, the user has opted to save the account, and that account has a username save the record.
+            else if (existingRecord == null && saveAccount && !string.IsNullOrWhiteSpace(userName))
+            {
+                _accounts.Add(new Account() { 
+                    UserName = userName,
+                    Server = serverName, 
+                    Password = password 
+                });
+            }
+            SaveAccountsToFile();
+        }
+
+        private static void LoadAccounts(string serverName)
+        {
+            if(_accounts != null)
+            {
+                return;
+            }
+
+            var accounts = LoadAccountsFromFile();
+            _accounts = accounts.Where(account => account.Server == serverName).ToList();
+        }
+
+        private static bool SaveAccountsToFile()
+        {
             try
             {
-                Load(serverName);
-                var existingRecord = Accounts.FirstOrDefault(x => x.Server == serverName && x.UserName == userName);
-                //There is an existing record and the user has opted to not save that account, remove from list.
-                if(existingRecord != null && !saveAccount)
-                {
-                    Accounts.Remove(existingRecord);
-                }
-                //There is an existing record and they have updated their password.  save the udpated password.
-                else if (existingRecord != null && existingRecord.Password != password)
-                {
-                    existingRecord.Password = password;
-                }
-                //Otherwise if there is no existing record, the user has opted to save the account, and that account has a username save the record.
-                else if (existingRecord == null && saveAccount && !string.IsNullOrWhiteSpace(userName))
-                {
-                    Accounts.Add(new Account() { UserName = userName, Server = serverName, Password = password });
-                }
-                ConfigurationResolver.Save(Accounts, PathToAccountFile());
+                ConfigurationResolver.Save(_accounts, PathToAccountFile());
+                return true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                //Log error?
+                Log.Error(ex.ToString());
+                return false;
             }
         }
 
-        private static void Load(string serverName)
-        {
-            if (Accounts == null)
-            {
-                var accounts = LoadAccountsFromFile();
-                Accounts = accounts.Where(x => x.Server == serverName).ToList();
-            }
-        }
         private static List<Account> LoadAccountsFromFile()
         {
-            var accounts = ConfigurationResolver.Load<List<Account>>(PathToAccountFile()) ?? new List<Account>();
-            return accounts;
+            try
+            {
+                var accounts = ConfigurationResolver.Load<List<Account>>(PathToAccountFile()) ?? new List<Account>();
+                return accounts;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+                return new List<Account>();
+            }
         }
 
         private static string PathToAccountFile()
