6af5f47ecf9f111fc7a0b203c4ae726e398dc03e
Quick <577652+markdwags@users.noreply.github.com>
2020-12-31 16:02:09 -0600

    Winter season will now change forest/grass tiles to the correct winter tile.
    - Jungle and sand remain unchanged but all mountains/coastlines/etc are changed as well. No proper snow/jungle and snow/sand tile exists

--- src/Game/Managers/SeasonManager.cs
+++ src/Game/Managers/SeasonManager.cs
@@ -6,37 +6,314 @@ namespace ClassicUO.Game.Managers
 
         static SeasonManager()
         {
-            _winterGraphic[196] = 0x011A;
-            _winterGraphic[197] = 0x011B;
-            _winterGraphic[198] = 0x011C;
-            _winterGraphic[199] = 0x011D;
-            _winterGraphic[206] = 0x05C0;
-            _winterGraphic[248] = 0x011A;
-            _winterGraphic[249] = 0x011B;
-            _winterGraphic[250] = 0x011C;
-            _winterGraphic[251] = 0x011D;
-            _winterGraphic[804] = 0x0391;
-            _winterGraphic[805] = 0x0392;
-            _winterGraphic[806] = 0x0393;
-            _winterGraphic[807] = 0x0394;
-            _winterGraphic[808] = 0x0395;
-            _winterGraphic[809] = 0x0396;
-            _winterGraphic[1521] = 0x011A;
-            _winterGraphic[1522] = 0x011B;
-            _winterGraphic[1523] = 0x011C;
-            _winterGraphic[1524] = 0x011D;
-            _winterGraphic[1529] = 0x011A;
-            _winterGraphic[1530] = 0x011B;
-            _winterGraphic[1531] = 0x011C;
-            _winterGraphic[1532] = 0x011D;
-            _winterGraphic[1533] = 0x011B;
-            _winterGraphic[1534] = 0x011C;
-            _winterGraphic[1535] = 0x011D;
-            _winterGraphic[1536] = 0x011B;
-            _winterGraphic[1537] = 0x011C;
-            _winterGraphic[1538] = 0x011D;
-            _winterGraphic[1539] = 0x011C;
-            _winterGraphic[1540] = 0x011D;
+            // solid forest tiles
+            _winterGraphic[196] = 282;
+            _winterGraphic[197] = 283;
+            _winterGraphic[198] = 284;
+            _winterGraphic[199] = 285;
+
+            _winterGraphic[248] = 282;
+            _winterGraphic[249] = 283;
+            _winterGraphic[250] = 284;
+            _winterGraphic[251] = 285;
+
+            _winterGraphic[349] = 937;
+            _winterGraphic[350] = 940;
+            _winterGraphic[351] = 938;
+            _winterGraphic[352] = 939;
+
+            // transition forest/grass tiles
+            _winterGraphic[200] = 282;
+            _winterGraphic[201] = 283;
+            _winterGraphic[202] = 284;
+            _winterGraphic[203] = 285;
+            _winterGraphic[204] = 282;
+            _winterGraphic[205] = 283;
+            _winterGraphic[206] = 284;
+            _winterGraphic[207] = 285;
+            _winterGraphic[208] = 282;
+            _winterGraphic[209] = 283;
+            _winterGraphic[210] = 284;
+            _winterGraphic[211] = 285;
+            _winterGraphic[212] = 282;
+            _winterGraphic[213] = 283;
+            _winterGraphic[214] = 284;
+            _winterGraphic[215] = 285;
+            _winterGraphic[216] = 282;
+            _winterGraphic[217] = 283;
+            _winterGraphic[218] = 284;
+            _winterGraphic[219] = 285;
+
+            _winterGraphic[1697] = 282;
+            _winterGraphic[1698] = 283;
+            _winterGraphic[1699] = 284;
+            _winterGraphic[1700] = 285;
+
+            _winterGraphic[1711] = 282;
+            _winterGraphic[1712] = 283;
+            _winterGraphic[1713] = 284;
+            _winterGraphic[1714] = 285;
+            _winterGraphic[1715] = 282;
+            _winterGraphic[1716] = 283;
+            _winterGraphic[1717] = 284;
+            _winterGraphic[1718] = 285;
+            _winterGraphic[1719] = 282;
+            _winterGraphic[1720] = 283;
+            _winterGraphic[1721] = 284;
+            _winterGraphic[1722] = 285;
+            _winterGraphic[1723] = 282;
+            _winterGraphic[1724] = 283;
+            _winterGraphic[1725] = 284;
+            _winterGraphic[1726] = 285;
+            _winterGraphic[1727] = 282;
+            _winterGraphic[1728] = 283;
+            _winterGraphic[1729] = 284;
+            _winterGraphic[1730] = 285;
+
+            // transition forest/dirt tiles
+            _winterGraphic[332] = 932;
+            _winterGraphic[333] = 929;
+            _winterGraphic[334] = 930;
+            _winterGraphic[335] = 931;
+
+            _winterGraphic[353] = 908;
+            _winterGraphic[354] = 907;
+            _winterGraphic[355] = 905;
+            _winterGraphic[356] = 906;
+            _winterGraphic[357] = 904;
+            _winterGraphic[358] = 903;
+            _winterGraphic[359] = 902;
+            _winterGraphic[360] = 901;
+
+            _winterGraphic[361] = 912;
+            _winterGraphic[362] = 911;
+            _winterGraphic[363] = 909;
+            _winterGraphic[364] = 910;
+
+            _winterGraphic[369] = 916;
+            _winterGraphic[370] = 915;
+            _winterGraphic[371] = 914;
+            _winterGraphic[372] = 913;
+
+            _winterGraphic[1351] = 917;
+            _winterGraphic[1352] = 918;
+            _winterGraphic[1353] = 919;
+            _winterGraphic[1354] = 920;
+            _winterGraphic[1355] = 921;
+            _winterGraphic[1356] = 922;
+            _winterGraphic[1357] = 923;
+            _winterGraphic[1358] = 924;
+            _winterGraphic[1359] = 925;
+
+            _winterGraphic[1360] = 927;
+            _winterGraphic[1361] = 928;
+            _winterGraphic[1362] = 930;
+
+            _winterGraphic[1363] = 933;
+            _winterGraphic[1364] = 934;
+            _winterGraphic[1365] = 935;
+            _winterGraphic[1366] = 936;
+
+            _winterGraphic[804] = 931;
+            _winterGraphic[805] = 929;
+            _winterGraphic[806] = 926;
+            _winterGraphic[807] = 925;
+            _winterGraphic[808] = 932;
+            _winterGraphic[809] = 930;
+            _winterGraphic[810] = 928;
+            _winterGraphic[811] = 927;
+            _winterGraphic[812] = 919;
+            _winterGraphic[813] = 920;
+            _winterGraphic[814] = 917;
+            _winterGraphic[815] = 921;
+
+            // solid grass tiles
+            _winterGraphic[3] = 282;
+            _winterGraphic[4] = 283;
+            _winterGraphic[5] = 284;
+            _winterGraphic[6] = 285;
+
+            // transition grass/dirt tiles
+            _winterGraphic[121] = 910;
+            _winterGraphic[122] = 909;
+            _winterGraphic[123] = 912;
+            _winterGraphic[124] = 911;
+
+            _winterGraphic[125] = 906;
+            _winterGraphic[126] = 905;
+            _winterGraphic[130] = 908;
+            _winterGraphic[131] = 907;
+
+            _winterGraphic[133] = 904;
+            _winterGraphic[134] = 904;
+
+            _winterGraphic[135] = 903;
+            _winterGraphic[136] = 903;
+
+            _winterGraphic[137] = 902;
+            _winterGraphic[138] = 902;
+
+            _winterGraphic[139] = 901;
+            _winterGraphic[140] = 901;
+
+            _winterGraphic[871] = 917;
+            _winterGraphic[872] = 918;
+            _winterGraphic[873] = 919;
+            _winterGraphic[874] = 920;
+            _winterGraphic[875] = 921;
+            _winterGraphic[876] = 922;
+            _winterGraphic[877] = 923;
+            _winterGraphic[878] = 924;
+            _winterGraphic[879] = 925;
+            _winterGraphic[880] = 926;
+            _winterGraphic[881] = 927;
+            _winterGraphic[882] = 928;
+            _winterGraphic[883] = 929;
+            _winterGraphic[884] = 930;
+            _winterGraphic[885] = 931;
+            _winterGraphic[886] = 932;
+            _winterGraphic[887] = 933;
+            _winterGraphic[888] = 934;
+            _winterGraphic[889] = 935;
+            _winterGraphic[890] = 936;
+            _winterGraphic[891] = 937;
+            _winterGraphic[892] = 938;
+            _winterGraphic[893] = 939;
+            _winterGraphic[894] = 940;
+
+            _winterGraphic[365] = 916;
+            _winterGraphic[366] = 915;
+            _winterGraphic[367] = 913;
+            _winterGraphic[368] = 914;
+
+
+            // transition forest/mountain tiles
+            _winterGraphic[236] = 278;
+            _winterGraphic[237] = 279;
+            _winterGraphic[238] = 276;
+            _winterGraphic[239] = 277;
+
+            _winterGraphic[240] = 305;
+            _winterGraphic[241] = 302;
+            _winterGraphic[242] = 303;
+            _winterGraphic[243] = 304;
+
+            _winterGraphic[244] = 272;
+            _winterGraphic[245] = 273;
+            _winterGraphic[246] = 274;
+            _winterGraphic[247] = 275;
+
+            // transition grass/mountain tiles
+            _winterGraphic[561] = 268;
+            _winterGraphic[562] = 269;
+            _winterGraphic[563] = 270;
+            _winterGraphic[564] = 271;
+            _winterGraphic[565] = 272;
+            _winterGraphic[566] = 273;
+            _winterGraphic[567] = 274;
+            _winterGraphic[568] = 275;
+            _winterGraphic[569] = 276;
+            _winterGraphic[570] = 277;
+            _winterGraphic[571] = 278;
+            _winterGraphic[572] = 279;
+
+            _winterGraphic[573] = 1861;
+            _winterGraphic[574] = 1862;
+            _winterGraphic[575] = 1863;
+            _winterGraphic[576] = 1864;
+            _winterGraphic[577] = 1865;
+            _winterGraphic[578] = 1866;
+            _winterGraphic[579] = 1867;
+
+            _winterGraphic[1741] = 1868;
+            _winterGraphic[1742] = 1869;
+            _winterGraphic[1743] = 1870;
+            _winterGraphic[1744] = 1871;
+            _winterGraphic[1745] = 1872;
+            _winterGraphic[1746] = 1873;
+            _winterGraphic[1747] = 1874;
+            _winterGraphic[1748] = 1875;
+            _winterGraphic[1749] = 1876;
+            _winterGraphic[1750] = 1877;
+            _winterGraphic[1751] = 1878;
+            _winterGraphic[1752] = 1879;
+            _winterGraphic[1753] = 1880;
+            _winterGraphic[1754] = 1881;
+            _winterGraphic[1755] = 1882;
+            _winterGraphic[1756] = 1883;
+            _winterGraphic[1757] = 1884;
+
+            _winterGraphic[1758] = 282;
+            _winterGraphic[1759] = 283;
+            _winterGraphic[1760] = 284;
+            _winterGraphic[1761] = 285;
+
+            // transition grass/coastline tiles
+            _winterGraphic[26] = 379;
+            _winterGraphic[27] = 378;
+            _winterGraphic[28] = 377;
+            _winterGraphic[29] = 380;
+            _winterGraphic[30] = 381;
+            _winterGraphic[31] = 382;
+            _winterGraphic[32] = 383;
+            _winterGraphic[33] = 384;
+            _winterGraphic[34] = 385;
+            _winterGraphic[35] = 386;
+            _winterGraphic[36] = 387;
+            _winterGraphic[37] = 388;
+            _winterGraphic[38] = 389;
+            _winterGraphic[39] = 390;
+            _winterGraphic[40] = 391;
+            _winterGraphic[41] = 392;
+            _winterGraphic[42] = 393;
+            _winterGraphic[43] = 394;
+
+            _winterGraphic[141] = 379;
+            _winterGraphic[142] = 386;
+            _winterGraphic[143] = 385;
+            _winterGraphic[144] = 393;
+
+            _winterGraphic[145] = 378;
+            _winterGraphic[146] = 387;
+            _winterGraphic[147] = 391;
+            _winterGraphic[148] = 392;
+
+            _winterGraphic[149] = 377;
+            _winterGraphic[150] = 379;
+            _winterGraphic[151] = 383;
+            _winterGraphic[152] = 380;
+            _winterGraphic[153] = 387;
+            _winterGraphic[154] = 388;
+            _winterGraphic[155] = 393;
+            _winterGraphic[156] = 391;
+            _winterGraphic[157] = 387;
+            _winterGraphic[158] = 385;
+            _winterGraphic[159] = 385;
+            _winterGraphic[160] = 389;
+            _winterGraphic[161] = 379;
+            _winterGraphic[162] = 384;
+            _winterGraphic[163] = 380;
+            _winterGraphic[164] = 379;
+            _winterGraphic[165] = 378;
+            _winterGraphic[166] = 378;
+            _winterGraphic[167] = 394;
+
+            _winterGraphic[1521] = 282;
+            _winterGraphic[1522] = 283;
+            _winterGraphic[1523] = 284;
+            _winterGraphic[1524] = 285;
+            _winterGraphic[1529] = 282;
+            _winterGraphic[1530] = 283;
+            _winterGraphic[1531] = 284;
+            _winterGraphic[1532] = 285;
+            _winterGraphic[1533] = 283;
+            _winterGraphic[1534] = 284;
+            _winterGraphic[1535] = 285;
+            _winterGraphic[1536] = 283;
+            _winterGraphic[1537] = 284;
+            _winterGraphic[1538] = 285;
+            _winterGraphic[1539] = 284;
+            _winterGraphic[1540] = 285;
         }
 
         public static ushort GetSeasonGraphic(Season season, ushort graphic)
