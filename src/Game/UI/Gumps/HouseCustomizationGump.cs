cd4702e669f5cabc0bd7fc192986815db77847d3
Andrea Angelini <andrea-angelini@outlook.it>
2021-04-25 16:03:07 +0200

    + fix index out of bounds

--- src/Game/UI/Gumps/HouseCustomizationGump.cs
+++ src/Game/UI/Gumps/HouseCustomizationGump.cs
@@ -723,7 +723,7 @@ namespace ClassicUO.Game.UI.Gumps
                 // disable scissor
                 _dataBox.Add(new ScissorControl(false));
             }
-            else if (_customHouseManager.Category >= 0 && _customHouseManager.Category <= HouseCustomizationManager.Walls.Count)
+            else if (_customHouseManager.Category >= 0 && _customHouseManager.Category < HouseCustomizationManager.Walls.Count)
             {
                 List<CustomHouseWall> vec = HouseCustomizationManager.Walls[_customHouseManager.Category].Items;
 
