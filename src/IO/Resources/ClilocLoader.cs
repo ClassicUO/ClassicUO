b2a5d3180751c1f68bf5591cbfe5f91e7d4e758d
Siva <84196371+andyten@users.noreply.github.com>
2021-05-23 23:29:37 +0800

    Fixed output of empty cliloc messages.

--- src/IO/Resources/ClilocLoader.cs
+++ src/IO/Resources/ClilocLoader.cs
@@ -90,6 +90,35 @@ namespace ClassicUO.IO.Resources
                         return;
                     }
 
+                    if (_cliloc != "Cliloc.enu")
+                    { 
+                        string enupath = UOFileManager.GetUOFilePath("Cliloc.enu");
+
+                        using (BinaryReader reader = new BinaryReader(new FileStream(enupath, FileMode.Open, FileAccess.Read)))
+                        {
+                            reader.ReadInt32();
+                            reader.ReadInt16();
+                            byte[] buffer = new byte[1024];
+
+                            while (reader.BaseStream.Length != reader.BaseStream.Position)
+                            {
+                                int number = reader.ReadInt32();
+                                byte flag = reader.ReadByte();
+                                int length = reader.ReadInt16();
+
+                                if (length > buffer.Length)
+                                {
+                                    buffer = new byte[(length + 1023) & ~1023];
+                                }
+
+                                reader.Read(buffer, 0, length);
+                                string text = string.Intern(Encoding.UTF8.GetString(buffer, 0, length));
+
+                                _entries[number] = text;
+                            }
+                        }
+                    }
+
                     using (BinaryReader reader = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
                     {
                         reader.ReadInt32();
