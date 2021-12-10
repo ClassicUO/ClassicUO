88bb5fa68176f9909dc56cc6d9959d7015f4ee2b
Andrea Angelini <andrea-angelini@outlook.it>
2021-12-10 15:12:33 +0100

    + fxc.exe

--- src/shaders/compile_shaders.bat
+++ src/shaders/compile_shaders.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\fxc.exe" /T fx_2_0  /Fo IsometricWorld.fxc IsometricWorld.fx
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\fxc.exe" /T fx_2_0  /Fo xBR.fxc xBR.fx
+fxc.exe /T fx_2_0  /Fo IsometricWorld.fxc IsometricWorld.fx
+fxc.exe /T fx_2_0  /Fo xBR.fxc xBR.fx
 
 pause
