ClassicUO/ClassicUO

commit ef33e7cbd3ba84b3395acf9aa68fff3eee72c663

Account for draw depth when determing which object the mouse is over

src/Game/Scenes/GameSceneDrawingSorting.cs
@@ -567,7 +567,17 @@ namespace ClassicUO.Game.Scenes
             // slow as fuck
             if (allowSelection && obj.Z <= _maxGroundZ && obj.AllowedToDraw && obj.CheckMouseSelection())
             {
-                SelectedObject.Object = obj;
+                if (SelectedObject.Object is GameObject prev)
+                {
+                    if (obj.CalculateDepthZ() >= prev.CalculateDepthZ())
+                    {
+                        SelectedObject.Object = obj;
+                    }
+                }
+                else
+                {
+                    SelectedObject.Object = obj;
+                }
             }
 
             if (obj.AlphaHue != byte.MaxValue)

commit a4e7f1d01eb40dcf2c4aff8a45749a410b89bbb1

+ mouse: fixed an issue when dropping an item grabbed from the paperdoll. The issue started after the PR https://github.com/ClassicUO/ClassicUO/pull/1496

src/Game/Managers/UIManager.cs
@@ -499,13 +499,18 @@ namespace ClassicUO.Game.Managers
 
             MouseOverControl = gump;
 
-            for (int i = 0; i < (int) MouseButtonType.Size; i++)
-            {
-                if (_mouseDownControls[i] != null && _mouseDownControls[i] != gump)
-                {
-                    _mouseDownControls[i].InvokeMouseOver(Mouse.Position);
-                }
-            }
+
+            // NOTE:
+            // This is causes the SelectObject.Object = null when dragging an item from the paperdoll
+            // I've no idea why i wrote this code.
+
+            //for (int i = 0; i < (int) MouseButtonType.Size; i++)
+            //{
+            //    if (_mouseDownControls[i] != null && _mouseDownControls[i] != gump)
+            //    {
+            //        _mouseDownControls[i].InvokeMouseOver(Mouse.Position);
+            //    }
+            //}
         }
 
         private static Control GetMouseOverControl(Point position)

src/Game/Scenes/GameScene.cs
@@ -621,6 +621,7 @@ namespace ClassicUO.Game.Scenes
             bool use_handles = _useObjectHandles;
             int maxCotZ = World.Player.Z + 5;
             Vector2 playerPos = World.Player.GetScreenPosition();
+            bool canMouseSelect = UIManager.IsMouseOverWorld;
 
             for (int i = 0; i < 2; ++i)
             {
@@ -654,7 +655,8 @@ namespace ClassicUO.Game.Scenes
                             use_handles,
                             150,
                             maxCotZ,
-                            ref playerPos
+                            ref playerPos,
+                            canMouseSelect
                         );
 
                         ++x;

src/Game/Scenes/GameSceneDrawingSorting.cs
@@ -622,7 +622,8 @@ namespace ClassicUO.Game.Scenes
             bool useObjectHandles, 
             int maxZ,
             int cotZ, 
-            ref Vector2 playerScreePos
+            ref Vector2 playerScreePos,
+            bool canMouseSelect
         )
         {
             for (; obj != null; obj = obj.TNext)
@@ -665,7 +666,7 @@ namespace ClassicUO.Game.Scenes
                         continue;
                     }
 
-                    PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, true);
+                    PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, canMouseSelect);
                 }
                 else if (obj is Static staticc)
                 {
@@ -719,7 +720,7 @@ namespace ClassicUO.Game.Scenes
                     // hacky way to render shadows without z-fight
                     if (ProfileManager.CurrentProfile.ShadowsEnabled && ProfileManager.CurrentProfile.ShadowsStatics && (StaticFilters.IsTree(obj.Graphic, out _) || itemData.IsFoliage || StaticFilters.IsRock(obj.Graphic)))
                     {
-                        PushToRenderList(obj, ref _renderListTransparentObjects, ref _renderListTransparentObjectsHead, ref _renderListTransparentObjectsCount, allowSelection);
+                        PushToRenderList(obj, ref _renderListTransparentObjects, ref _renderListTransparentObjectsHead, ref _renderListTransparentObjectsCount, allowSelection && canMouseSelect);
                     }
                     else
                     {
@@ -731,7 +732,7 @@ namespace ClassicUO.Game.Scenes
                             obj.AlphaHue = 0xFF;
                         }
 
-                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, allowSelection);
+                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, allowSelection && canMouseSelect);
 
                         obj.AlphaHue = alpha;
                     } 
@@ -787,7 +788,7 @@ namespace ClassicUO.Game.Scenes
                     // hacky way to render shadows without z-fight
                     if (ProfileManager.CurrentProfile.ShadowsEnabled && ProfileManager.CurrentProfile.ShadowsStatics && (StaticFilters.IsTree(obj.Graphic, out _) || itemData.IsFoliage || StaticFilters.IsRock(obj.Graphic)))
                     {
-                        PushToRenderList(obj, ref _renderListTransparentObjects, ref _renderListTransparentObjectsHead, ref _renderListTransparentObjectsCount, allowSelection);
+                        PushToRenderList(obj, ref _renderListTransparentObjects, ref _renderListTransparentObjectsHead, ref _renderListTransparentObjectsCount, allowSelection && canMouseSelect);
                     }
                     else
                     {
@@ -799,7 +800,7 @@ namespace ClassicUO.Game.Scenes
                             obj.AlphaHue = 0xFF;
                         }
 
-                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, allowSelection);
+                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, allowSelection && canMouseSelect);
 
                         obj.AlphaHue = alpha;
                     }
@@ -829,7 +830,7 @@ namespace ClassicUO.Game.Scenes
 
                     obj.AllowedToDraw = !HasSurfaceOverhead(mobile);
 
-                    PushToRenderList(obj, ref _renderListAnimations, ref _renderListAnimationsHead, ref _renderListAnimationCount, allowSelection);
+                    PushToRenderList(obj, ref _renderListAnimations, ref _renderListAnimationsHead, ref _renderListAnimationCount, allowSelection && canMouseSelect);
                 }
                 else if (obj is Item item)
                 {
@@ -891,11 +892,11 @@ namespace ClassicUO.Game.Scenes
 
                     if (item.IsCorpse)
                     {
-                        PushToRenderList(obj, ref _renderListAnimations, ref _renderListAnimationsHead, ref _renderListAnimationCount, allowSelection);
+                        PushToRenderList(obj, ref _renderListAnimations, ref _renderListAnimationsHead, ref _renderListAnimationCount, allowSelection && canMouseSelect);
                     }
                     else
                     {
-                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, true);
+                        PushToRenderList(obj, ref _renderList, ref _renderListStaticsHead, ref _renderListStaticsCount, canMouseSelect);
                     }         
                 }
                 else if (obj is GameEffect effect)

src/Game/Scenes/GameSceneInputHandler.cs
@@ -434,7 +434,7 @@ namespace ClassicUO.Game.Scenes
                 ushort dropY = 0;
                 sbyte dropZ = 0;
 
-                GameObject gobj = SelectedObject.Object as GameObject;
+                GameObject gobj = lastObj as GameObject;
 
                 if (gobj is Entity obj)
                 {
@@ -609,14 +609,14 @@ namespace ClassicUO.Game.Scenes
 
                     case CursorTarget.HueCommandTarget:
 
-                        if (SelectedObject.Object is Entity selectedEntity)
+                        if (lastObj is Entity selectedEntity)
                         {
                             CommandManager.OnHueTarget(selectedEntity);
                         }
 
                         break;
                     case CursorTarget.IgnorePlayerTarget:
-                        if (SelectedObject.Object is Entity pmEntity)
+                        if (lastObj is Entity pmEntity)
                         {
                             IgnoreManager.AddIgnoredTarget(pmEntity);
                         }