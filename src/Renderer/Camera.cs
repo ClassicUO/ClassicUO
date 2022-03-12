429fdef934df49b4ba662ee39147c7bdb189cace
Andrea Angelini <andrea-angelini@outlook.it>
2022-03-12 20:09:15 +0100

    + camera: initial zoom

--- src/Renderer/Camera.cs
+++ src/Renderer/Camera.cs
@@ -52,6 +52,7 @@ namespace ClassicUO.Renderer
             ZoomMin = minZoomValue;
             ZoomMax = maxZoomValue;
             ZoomStep = zoomStep;
+            Zoom = _lerpZoom = 1f;
         }
 
 
@@ -175,15 +176,9 @@ namespace ClassicUO.Renderer
 
             Matrix.CreateTranslation(-origin.X, -origin.Y, 0f, out _transform);
 
-            float zoom = 1f / Zoom;
+            CalculateLerpZoom();
 
-            const float FADE_TIME = 12.0f;
-            const float SMOOTHING_FACTOR = (1.0f / FADE_TIME) * 60.0f;
-
-            _lerpZoom = MathHelper.Lerp(_lerpZoom, zoom, SMOOTHING_FACTOR * Time.Delta);
-            zoom = _lerpZoom;
-
-            Matrix.CreateScale(zoom, zoom, 1f, out temp);
+            Matrix.CreateScale(_lerpZoom, _lerpZoom, 1f, out temp);
             Matrix.Multiply(ref _transform, ref temp, out _transform);
 
             Matrix.CreateTranslation(origin.X, origin.Y, 0f, out temp);
@@ -193,6 +188,16 @@ namespace ClassicUO.Renderer
             Matrix.Invert(ref _transform, out _inverseTransform);
 
             _updateMatrixes = false;
+        }
+
+        private void CalculateLerpZoom()
+        {
+            float zoom = 1f / Zoom;
+
+            const float FADE_TIME = 12.0f;
+            const float SMOOTHING_FACTOR = (1.0f / FADE_TIME) * 60.0f;
+
+            _lerpZoom = MathHelper.Lerp(_lerpZoom, zoom, SMOOTHING_FACTOR * Time.Delta);
         }
     }
 }
