a9b1543afdc944de7644038debc370f6580f0ff9
Andrea Angelini <andrea-angelini@outlook.it>
2022-03-30 17:49:02 +0100

    + disable the zoom effect: i can cause eye discomfort

--- src/Renderer/Camera.cs
+++ src/Renderer/Camera.cs
@@ -171,7 +171,7 @@ namespace ClassicUO.Renderer
             const float FADE_TIME = 12.0f;
             const float SMOOTHING_FACTOR = (1.0f / FADE_TIME) * 60.0f;
 
-            _lerpZoom = MathHelper.Lerp(_lerpZoom, zoom, SMOOTHING_FACTOR * Time.Delta);
+            _lerpZoom = zoom; // MathHelper.Lerp(_lerpZoom, zoom, SMOOTHING_FACTOR * Time.Delta);
         }
     }
 }
