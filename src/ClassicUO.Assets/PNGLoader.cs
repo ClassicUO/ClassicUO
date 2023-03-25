11a2cfcdb7afa31e9afeb6bbf717eb76d88a2e37
Tad Taylor <bittiez@gmail.com>
2023-03-25 09:20:49 -0600

    Intial testing of this idea

---
+++ src/ClassicUO.Assets/PNGLoader.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ClassicUO.Assets
+{
+    public static class PNGLoader
+    {
+        public static Texture2D GetImageTexture(string fullImagePath, GraphicsDevice graphicsDevice)
+        {
+            Texture2D texture = null;
+
+            if (File.Exists(fullImagePath))
+            {
+                FileStream titleStream = File.OpenRead(fullImagePath);
+                texture = Texture2D.FromStream(graphicsDevice, titleStream);
+                titleStream.Close();
+                Color[] buffer = new Color[texture.Width * texture.Height];
+                texture.GetData(buffer);
+                for (int i = 0; i < buffer.Length; i++)
+                    buffer[i] = Color.FromNonPremultiplied(buffer[i].R, buffer[i].G, buffer[i].B, buffer[i].A);
+                texture.SetData(buffer);
+            }
+
+            return texture;
+        }
+    }
+}
