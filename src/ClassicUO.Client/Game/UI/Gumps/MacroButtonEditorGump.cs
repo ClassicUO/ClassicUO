2b7fb75b68205e83847fabc6329dffe3e129ccd3
Tad Taylor <bittiez@gmail.com>
2023-03-31 18:28:13 -0600

    Update MacroButtonEditorGump.cs

--- src/ClassicUO.Client/Game/UI/Gumps/MacroButtonEditorGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/MacroButtonEditorGump.cs
@@ -175,7 +175,7 @@ namespace ClassicUO.Game.UI.Gumps
 
             Label _ScaleLbl = new Label
             (
-                    $"Scale",
+                    "Scale",
                     true,
                     0xFFFF,
                     50,
