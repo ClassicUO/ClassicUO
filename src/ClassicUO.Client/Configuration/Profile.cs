a411982da2646f0749518255051e6e2bb6ef1553
elderwyn <arithas@gmail.com>
2023-04-04 00:51:08 -0400

    Durability Gump

--- src/ClassicUO.Client/Configuration/Profile.cs
+++ src/ClassicUO.Client/Configuration/Profile.cs
@@ -701,6 +701,9 @@ namespace ClassicUO.Configuration
                                     ushort ogContainer = ushort.Parse(xml.GetAttribute("ogContainer"));
                                     gump = new GridContainer(serial, ogContainer);
                                     break;
+                                case GumpType.DurabilityGump:
+                                    gump = new DurabilitysGump();
+                                    break;
                             }
 
                             if (gump == null)
