5756924e3cee6ca158c71c1ef69541439b06b05e
Tad Taylor <bittiez@gmail.com>
2023-04-21 04:29:17 -0600

    Create GumpLockButton.cs

---
+++ src/ClassicUO.Client/Game/UI/Controls/GumpLockButton.cs
@@ -0,0 +1,62 @@
+﻿using ClassicUO.Game.Managers;
+using ClassicUO.Input;
+using ClassicUO.Renderer;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ClassicUO.Game.UI.Controls
+{
+    internal class GumpLockButton : Control
+    {
+        private bool isLocked;
+
+        private Rectangle lockIconBounds;
+        private Texture2D lockIcon;
+        Vector3 hueLocked = Renderer.ShaderHueTranslator.GetHueVector(32);
+        Vector3 hueUnlocked = Renderer.ShaderHueTranslator.GetHueVector(0);
+
+        public GumpLockButton(ref bool isLocked)
+        {
+            this.isLocked = isLocked;
+            lockIcon = Assets.GumpsLoader.Instance.GetGumpTexture(0x82C, out lockIconBounds);
+        }
+
+        protected override void OnMouseUp(int x, int y, MouseButtonType button)
+        {
+            base.OnMouseUp(x, y, button);
+
+            if (button == MouseButtonType.Left && Keyboard.Alt && UIManager.MouseOverControl != null && (UIManager.MouseOverControl == this || UIManager.MouseOverControl.RootParent == this))
+            {
+                if (lockIcon != null)
+                {
+                    if (x >= 0 && x < lockIconBounds.Width && y >= 0 && y <= lockIconBounds.Height)
+                    {
+                        isLocked ^= true;
+                        if (isLocked)
+                        {
+                            CanMove = false;
+                            CanCloseWithRightClick = false;
+                        }
+                        else
+                        {
+                            CanMove = true;
+                            CanCloseWithRightClick = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override bool Draw(UltimaBatcher2D batcher, int x, int y)
+        {
+            base.Draw(batcher, x, y);
+
+            if (lockIcon != null && Keyboard.Alt && UIManager.MouseOverControl != null && (UIManager.MouseOverControl == this || UIManager.MouseOverControl.RootParent == this))
+            {
+                batcher.Draw(lockIcon, new Vector2(x, y), lockIconBounds, isLocked ? hueLocked : hueUnlocked);
+            }
+
+            return true;
+        }
+    }
+}
