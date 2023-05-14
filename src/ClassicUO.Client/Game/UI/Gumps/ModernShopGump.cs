20140706db3b8d009aa1c215e824886ad9a1cc51
Tad Taylor <bittiez@gmail.com>
2023-05-14 08:29:34 -0600

    ModernShop Gump changes

--- src/ClassicUO.Client/Game/UI/Gumps/ModernShopGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/ModernShopGump.cs
@@ -4,6 +4,7 @@ using ClassicUO.Game.Data;
 using ClassicUO.Game.GameObjects;
 using ClassicUO.Game.UI.Controls;
 using ClassicUO.Input;
+using ClassicUO.Network;
 using ClassicUO.Renderer;
 using Microsoft.Xna.Framework;
 using System;
@@ -19,7 +20,7 @@ namespace ClassicUO.Game.UI.Gumps
         private int WIDTH = 450;
 
         const int ITEM_DESCPTION_WIDTH = 300;
-
+        private readonly bool isPurchaseGump;
         private ScrollArea scrollArea;
         private StbTextBox searchBox;
         private AlphaBlendControl background;
@@ -35,11 +36,11 @@ namespace ClassicUO.Game.UI.Gumps
 
         public ModernShopGump(uint serial, bool isPurchaseGump) : base(serial, 0)
         {
-            if (!ProfileManager.CurrentProfile.EnableModernShopPreview)
-            {
-                Dispose();
-                return;
-            }
+            //if (!ProfileManager.CurrentProfile.EnableModernShopPreview)
+            //{
+            //    Dispose();
+            //    return;
+            //}
 
             #region VARS
             X = 200;
@@ -52,6 +53,8 @@ namespace ClassicUO.Game.UI.Gumps
             AcceptMouseInput = true;
             CanCloseWithRightClick = true;
             CanMove = true;
+
+            this.isPurchaseGump = isPurchaseGump;
             #endregion
 
             scrollArea = new ScrollArea(1, 75, Width - 2, Height - 77, true) { ScrollbarBehaviour = ScrollbarBehaviour.ShowAlways };
@@ -146,11 +149,11 @@ namespace ClassicUO.Game.UI.Gumps
         {
             if (IsDisposed)
                 return;
-            ShopItem _ = new ShopItem(serial, graphic, hue, amount, price, name, scrollArea.Width - scrollArea.ScrollBarWidth(), 50);
+            ShopItem _ = new ShopItem(serial, graphic, hue, amount, price, name, scrollArea.Width - scrollArea.ScrollBarWidth(), 50, isPurchaseGump, LocalSerial);
             _.Y = itemY;
             scrollArea.Add(_);
             shopItems.Add(_);
-            itemY += 50;
+            itemY += _.Height;
         }
 
         private void SearchContents(string text)
@@ -171,7 +174,7 @@ namespace ClassicUO.Game.UI.Gumps
                 {
                     i.Y = itemY;
                     scrollArea.Add(i);
-                    itemY += 50;
+                    itemY += i.Height;
                 }
             }
         }
@@ -179,7 +182,11 @@ namespace ClassicUO.Game.UI.Gumps
         private class ShopItem : Control
         {
             AlphaBlendControl backgound;
-            public ShopItem(uint serial, ushort graphic, ushort hue, int count, uint price, string name, int width, int height)
+            Area itemInfo, purchaseSell;
+            BuySellButton buySellButton;
+            private readonly uint gumpSerial;
+
+            public ShopItem(uint serial, ushort graphic, ushort hue, int count, uint price, string name, int width, int height, bool isPurchase, uint gumpSerial)
             {
                 Serial = serial;
                 Graphic = graphic;
@@ -187,6 +194,7 @@ namespace ClassicUO.Game.UI.Gumps
                 Count = count;
                 Price = price;
                 Name = name;
+                this.gumpSerial = gumpSerial;
                 Width = width;
                 Height = height;
 
@@ -196,20 +204,95 @@ namespace ClassicUO.Game.UI.Gumps
 
                 SetTooltip(serial);
 
+
+
                 Add(backgound = new AlphaBlendControl(0.01f) { Width = Width, Height = Height });
 
                 Add(new ResizableStaticPic(Graphic, Height, Height) { Hue = hue, AcceptMouseInput = false });
 
+                itemInfo = new Area(false) { Width = Width - Height, Height = Height, X = Height, AcceptMouseInput = false };
+                purchaseSell = new Area(false) { Width = Width - Height, Height = Height, X = Height, AcceptMouseInput = false, IsVisible = false };
+
+                #region ITEM INFO
                 TextBox _;
-                Add(_ = new TextBox(Name, TrueTypeLoader.EMBEDDED_FONT, 25, ITEM_DESCPTION_WIDTH, Color.White, dropShadow: true) { X = 51 });
-                _.Y = (Height - _.MeasuredSize.Y) / 2;
+                itemInfo.Add(_ = new TextBox(Name, TrueTypeLoader.EMBEDDED_FONT, 25, ITEM_DESCPTION_WIDTH - Height, Color.White, dropShadow: true));
+                _.Y = (itemInfo.Height - _.MeasuredSize.Y) / 2;
 
-                Add(_ = new TextBox($"x{count}", TrueTypeLoader.EMBEDDED_FONT, 20, (Width - ITEM_DESCPTION_WIDTH - 55), Color.WhiteSmoke, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = _.X + _.Width - 3, Y = 3 });
+                TextBox countTB;
+                itemInfo.Add(countTB = new TextBox($"x{count}", TrueTypeLoader.EMBEDDED_FONT, 20, ITEM_DESCPTION_WIDTH - Height, Color.WhiteSmoke, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { Y = 3 });
+                countTB.X = itemInfo.Width - countTB.Width - 3;
 
-                Add(_ = new TextBox($"{price}gp", TrueTypeLoader.EMBEDDED_FONT, 25, 300, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = Width - 303 });
-                _.Y = height - _.Height - 3;
+                itemInfo.Add(_ = new TextBox($"{price}gp", TrueTypeLoader.EMBEDDED_FONT, 25, ITEM_DESCPTION_WIDTH - Height, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Right, true));
+                _.Y = itemInfo.Height - _.Height - 3;
+                _.X = itemInfo.Width - _.Width - 3;
+                #endregion
+
+                #region PURCHASE OR SELL
+                purchaseSell.Add(new TextBox($"How many would you like to {(isPurchase ? "buy" : "sell")} at /c[gold]{price}gp /cdeach?", TrueTypeLoader.EMBEDDED_FONT, 18, purchaseSell.Width - 76, Color.White, dropShadow: true));
+
+                AlphaBlendControl sliderBG;
+                purchaseSell.Add(sliderBG = new AlphaBlendControl(0.5f) { Width = purchaseSell.Width - 78, Height = 5, Hue = 148, BaseColor = Color.White });
+
+                HSliderBar quantity;
+                purchaseSell.Add(quantity = new HSliderBar(3, 0, purchaseSell.Width - 78, 1, Count, 1, HSliderBarStyle.BlueWidgetNoBar));
+                quantity.Y = purchaseSell.Height - quantity.Height;
+                sliderBG.Y = quantity.Y + (quantity.Height / 2) - 2;
+
+                quantity.ValueChanged += (sender, e) =>
+                {
+                    buySellButton.UpdateQuantity(quantity.Value, (int)price);
+                };
+
+
+                purchaseSell.Add(buySellButton = new BuySellButton(isPurchase, 1, (int)price) { X = purchaseSell.Width - 75 });
+
+                buySellButton.MouseUp += (sender, e) =>
+                {
+                    Dictionary<uint, ushort> theItem = new Dictionary<uint, ushort>();
+                    theItem.Add(serial, (ushort)quantity.Value);
+
+                    Tuple<uint, ushort>[] item = theItem.Select(t => new Tuple<uint, ushort>(t.Key, (ushort)t.Value)).ToArray();
+                  
+                    if (isPurchase)
+                    {
+                        NetClient.Socket.Send_BuyRequest(gumpSerial, item);
+                        count -= quantity.Value;
+                    }
+                    else
+                    {
+                        NetClient.Socket.Send_SellRequest(gumpSerial, item);
+                        count -= quantity.Value;
+                    }
+
+                    if (count < 1)
+                        Dispose();
+                    else
+                    {
+                        quantity.MaxValue = count;
+                        quantity.Value = 1;
+                        countTB.Text = $"x{count}";
+                    }
+                };
+
+                #endregion
+
+                Add(itemInfo);
+                Add(purchaseSell);
 
                 Add(new SimpleBorder() { Width = Width, Height = Height, Hue = 0, Alpha = 0.2f });
+            }
+
+            protected override bool OnMouseDoubleClick(int x, int y, MouseButtonType button)
+            {
+                base.OnMouseDoubleClick(x, y, button);
+
+                if (button == MouseButtonType.Left)
+                {
+                    itemInfo.IsVisible ^= true;
+                    purchaseSell.IsVisible ^= true;
+                }
+
+                return true;
             }
 
             public override bool Draw(UltimaBatcher2D batcher, int x, int y)
@@ -231,6 +314,57 @@ namespace ClassicUO.Game.UI.Gumps
                         return true;
                 }
                 return false;
+            }
+
+            private class BuySellButton : Control
+            {
+                private readonly bool isPurchase;
+                TextBox text;
+
+                public BuySellButton(bool isPurchase, int quantity, int price)
+                {
+                    Width = 75;
+                    Height = 50;
+                    AcceptMouseInput = true;
+                    CanCloseWithRightClick = true;
+                    this.isPurchase = isPurchase;
+
+                    Add(new AlphaBlendControl() { Width = Width, Height = Height });
+
+                    UpdateQuantity(quantity, price);
+
+                    Add(new SimpleBorder() { Width = Width, Height = Height, Hue = 997, Alpha = 0.2f });
+
+                }
+
+                public void UpdateQuantity(int quantity, int price)
+                {
+                    if (text == null)
+                    {
+                        text = new TextBox($"{(isPurchase ? "Buy" : "Sell")}\n{quantity}\n/c[Gold]{price * quantity}", TrueTypeLoader.EMBEDDED_FONT, 17, Width, Color.White, FontStashSharp.RichText.TextHorizontalAlignment.Center, true);
+                        Add(text);
+                    }
+                    else
+                    {
+                        text.Text = $"{(isPurchase ? "Buy" : "Sell")}\n{quantity}\n/c[Gold]{price * quantity}";
+                    }
+                }
+
+                public override bool Draw(UltimaBatcher2D batcher, int x, int y)
+                {
+                    base.Draw(batcher, x, y);
+
+                    if (MouseIsOver)
+                    {
+                        batcher.Draw(
+                            SolidColorTextureCache.GetTexture(Color.White),
+                            new Rectangle(x, y, Width, Height),
+                            new Vector3(0, 0, 0.2f)
+                            );
+                    }
+
+                    return true;
+                }
             }
 
             public uint Serial { get; }
