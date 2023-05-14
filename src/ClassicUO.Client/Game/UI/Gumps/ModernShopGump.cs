5dcc8c3a12874bf7bf77db5e7d78a584925cdc1f
Tad Taylor <bittiez@gmail.com>
2023-05-14 05:52:04 -0600

    Update ModernShopGump.cs

--- src/ClassicUO.Client/Game/UI/Gumps/ModernShopGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/ModernShopGump.cs
@@ -3,6 +3,7 @@ using ClassicUO.Configuration;
 using ClassicUO.Game.Data;
 using ClassicUO.Game.GameObjects;
 using ClassicUO.Game.UI.Controls;
+using ClassicUO.Input;
 using ClassicUO.Renderer;
 using Microsoft.Xna.Framework;
 using System;
@@ -15,13 +16,18 @@ namespace ClassicUO.Game.UI.Gumps
 {
     internal class ModernShopGump : Gump
     {
-        const int WIDTH = 450;
-        const int HEIGHT = 700;
+        private int WIDTH = 450;
 
         const int ITEM_DESCPTION_WIDTH = 300;
 
         private ScrollArea scrollArea;
         private StbTextBox searchBox;
+        private AlphaBlendControl background;
+        private SimpleBorder border;
+
+        private HitBox resizeDrag;
+        private bool dragging = false;
+        private int dragStartH = 0;
 
         private List<ShopItem> shopItems = new List<ShopItem>();
 
@@ -39,7 +45,9 @@ namespace ClassicUO.Game.UI.Gumps
             X = 200;
             Y = 200;
             Width = WIDTH;
-            Height = HEIGHT;
+            Height = ProfileManager.CurrentProfile.VendorGumpHeight;
+            if (Height < 200)
+                Height = 200;
 
             AcceptMouseInput = true;
             CanCloseWithRightClick = true;
@@ -48,18 +56,18 @@ namespace ClassicUO.Game.UI.Gumps
 
             scrollArea = new ScrollArea(1, 75, Width - 2, Height - 77, true) { ScrollbarBehaviour = ScrollbarBehaviour.ShowAlways };
 
-            Add(new AlphaBlendControl(0.75f) { Width = Width, Height = Height, Hue = 999 });
+            Add(background = new AlphaBlendControl(0.75f) { Width = Width, Height = Height, Hue = 997 });
 
-            Add(new AlphaBlendControl(0.65f) { Width = Width, Height = 75, Hue = 999 });
+            Add(new AlphaBlendControl(0.65f) { Width = Width, Height = 75, Hue = 997 });
 
             TextBox _;
-            Add(_ = new TextBox(isPurchaseGump ? "Shop Inventory" : "Your Inventory", TrueTypeLoader.EMBEDDED_FONT, 30, Width, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Center, true));
+            Add(_ = new TextBox(isPurchaseGump ? "Shop Inventory" : "Your Inventory", TrueTypeLoader.EMBEDDED_FONT, 30, Width, Color.LightBlue, FontStashSharp.RichText.TextHorizontalAlignment.Center, true));
             _.Y = (50 - _.MeasuredSize.Y) / 2;
 
             Add(_ = new TextBox("Item", TrueTypeLoader.EMBEDDED_FONT, 20, 75, Color.White, dropShadow: true) { X = 5 });
             _.Y = 55 - _.Height;
 
-            Add(_ = new TextBox("Avail.\nPrice per item", TrueTypeLoader.EMBEDDED_FONT, 20, 150, Color.White, FontStashSharp.RichText.TextHorizontalAlignment.Right, true));
+            Add(_ = new TextBox("/c[white]Avail.\n/cdPrice per item", TrueTypeLoader.EMBEDDED_FONT, 20, 150, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Right, true));
             _.Y = 55 - _.Height;
             _.X = Width - 1 - _.Width - 5;
 
@@ -68,7 +76,7 @@ namespace ClassicUO.Game.UI.Gumps
                 X = 1,
                 Y = 55,
                 Multiline = false,
-                Width = Width - 2,
+                Width = Width - 1,
                 Height = 20
             };
             searchBox.TextChanged += (s, e) =>
@@ -86,7 +94,43 @@ namespace ClassicUO.Game.UI.Gumps
 
             Add(scrollArea);
 
-            Add(new SimpleBorder() { Width = Width, Height = Height, Hue = 0, Alpha = 0.3f });
+            Add(resizeDrag = new HitBox(Width / 2 - 10, Height - 10, 20, 10, "Drag to resize", 0.50f));
+            resizeDrag.Add(new AlphaBlendControl(0.5f) { Width = 20, Height = 10, Hue = 997 });
+            resizeDrag.MouseDown += ResizeDrag_MouseDown;
+            resizeDrag.MouseUp += ResizeDrag_MouseUp;
+
+            Add(border = new SimpleBorder() { Width = Width, Height = Height, Hue = 0, Alpha = 0.3f });
+        }
+
+        private void ResizeDrag_MouseUp(object sender, Input.MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void ResizeDrag_MouseDown(object sender, Input.MouseEventArgs e)
+        {
+            dragStartH = Height;
+            dragging = true;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            int steps = Mouse.LDragOffset.Y;
+
+            if (dragging && steps != 0)
+            {
+                Height = dragStartH + steps;
+                if (Height < 200)
+                    Height = 200;
+                ProfileManager.CurrentProfile.VendorGumpHeight = Height;
+
+                background.Height = Height;
+                scrollArea.Height = Height - 77;
+                resizeDrag.Y = Height - 10;
+                border.Height = Height;
+            }
         }
 
         public void AddItem
@@ -160,10 +204,10 @@ namespace ClassicUO.Game.UI.Gumps
                 Add(_ = new TextBox(Name, TrueTypeLoader.EMBEDDED_FONT, 25, ITEM_DESCPTION_WIDTH, Color.White, dropShadow: true) { X = 51 });
                 _.Y = (Height - _.MeasuredSize.Y) / 2;
 
-                Add(_ = new TextBox($"x{count}", TrueTypeLoader.EMBEDDED_FONT, 25, (Width - ITEM_DESCPTION_WIDTH - 55), Color.WhiteSmoke, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = _.X + _.Width });
+                Add(_ = new TextBox($"x{count}", TrueTypeLoader.EMBEDDED_FONT, 20, (Width - ITEM_DESCPTION_WIDTH - 55), Color.WhiteSmoke, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = _.X + _.Width - 3, Y = 3 });
 
-                Add(_ = new TextBox($"{price}gp", TrueTypeLoader.EMBEDDED_FONT, 25, 300, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = Width - 300 });
-                _.Y = height - _.Height;
+                Add(_ = new TextBox($"{price}gp", TrueTypeLoader.EMBEDDED_FONT, 25, 300, Color.Gold, FontStashSharp.RichText.TextHorizontalAlignment.Right, true) { X = Width - 303 });
+                _.Y = height - _.Height - 3;
 
                 Add(new SimpleBorder() { Width = Width, Height = Height, Hue = 0, Alpha = 0.2f });
             }
