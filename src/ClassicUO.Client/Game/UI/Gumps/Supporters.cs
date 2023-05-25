e0bc266ee2450cb9ba294c93d4dd7ac8a49f4adf
Tad Taylor <bittiez@gmail.com>
2023-05-25 17:13:28 -0600

    Update Supporters.cs

--- src/ClassicUO.Client/Game/UI/Gumps/Supporters.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/Supporters.cs
@@ -21,7 +21,8 @@ namespace ClassicUO.Game.UI.Gumps
             "Auburok - Don't leave Brit Bank without TazUO",
             "IDiivil - Happily Organized Now",
             "Avernal",
-            "d6punk - UO for life!"
+            "d6punk - UO for life!",
+            "Eora"
         };
         private AlphaBlendControl _background;
 
