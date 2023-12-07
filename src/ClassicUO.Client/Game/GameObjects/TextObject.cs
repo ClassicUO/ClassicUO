c0b9ac7b9d58606ee8044567f6d5417858582ffb
Andrea Angelini <andrea-angelini@outlook.it>
2023-12-07 15:22:24 +0100

    alpha 1

--- src/ClassicUO.Client/Game/GameObjects/TextObject.cs
+++ src/ClassicUO.Client/Game/GameObjects/TextObject.cs
@@ -2,7 +2,7 @@
 
 // Copyright (c) 2021, andreakarasho
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 1. Redistributions of source code must retain the above copyright
@@ -16,7 +16,7 @@
 // 4. Neither the name of the copyright holder nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -83,7 +83,7 @@ namespace ClassicUO.Game.GameObjects
 
         public static TextObject Create(World world)
         {
-            return new TextObject(world); // _queue.GetOne();
+            return new TextObject(world) { Alpha = 0xFF }; // _queue.GetOne();
         }
 
 
