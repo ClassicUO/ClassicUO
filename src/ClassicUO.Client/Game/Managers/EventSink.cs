1f7c579b08fb39f2e7ceac0e47752b00138325b9
Tad Taylor <bittiez@gmail.com>
2023-12-25 11:43:06 -0700

    Added some summaries

--- src/ClassicUO.Client/Game/Managers/EventSink.cs
+++ src/ClassicUO.Client/Game/Managers/EventSink.cs
@@ -4,21 +4,39 @@ namespace ClassicUO.Game.Managers
 {
     public class EventSink
     {
+        /// <summary>
+        /// Invoked when the player is connected to a server
+        /// </summary>
         public static event EventHandler<EventArgs> OnConnected;
         public static void InvokeOnConnected(object sender) => OnConnected?.Invoke(sender, EventArgs.Empty);
 
+        /// <summary>
+        /// Invoked when any message is received from the server after client processing
+        /// </summary>
         public static event EventHandler<MessageEventArgs> MessageReceived;
         public static void InvokeMessageReceived(object sender, MessageEventArgs e) => MessageReceived?.Invoke(sender, e);
 
+        /// <summary>
+        /// Invoked when any message is received from the server *before* client processing
+        /// </summary>
         public static event EventHandler<MessageEventArgs> RawMessageReceived;
         public static void InvokeRawMessageReceived(object sender, MessageEventArgs e) => RawMessageReceived?.Invoke(sender, e);
 
+        /// <summary>
+        /// Not currently used. May be removed later or put into use, not sure right now
+        /// </summary>
         public static event EventHandler<MessageEventArgs> LocalizedMessageReceived;
         public static void InvokeLocalizedMessageReceived(object sender, MessageEventArgs e) => LocalizedMessageReceived?.Invoke(sender, e);
 
+        /// <summary>
+        /// Invoked anytime a message is added to the journal
+        /// </summary>
         public static event EventHandler<JournalEntry> JournalEntryAdded;
         public static void InvokeJournalEntryAdded(object sender, JournalEntry e) => JournalEntryAdded?.Invoke(sender, e);
 
+        /// <summary>
+        /// Invoked anytime we receive object property list data (Tooltip text for items)
+        /// </summary>
         public static event EventHandler<OPLEventArgs> OPLOnReceive;
         public static void InvokeOPLOnReceive(object sender, OPLEventArgs e) => OPLOnReceive?.Invoke(sender, e);
     }
