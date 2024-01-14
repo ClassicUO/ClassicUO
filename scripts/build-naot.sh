0ef999dc6a19ac8891606b7be64df87699aa1985
andreakarasho <andrea-angelini@outlook.it>
2024-01-14 19:41:26 +0100

    script

--- scripts/build-naot.sh
+++ scripts/build-naot.sh
@@ -15,12 +15,12 @@ case $platform in
   Linux)
     # Add Linux-specific build commands here
     dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
-    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory" 
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r linux-x64 -o "$output_directory" 
     ;;
   Darwin)
     # Add macOS-specific build commands here
     dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
-    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory" 
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r osx-x64 -o "$output_directory" 
     ;;
   MINGW* | CYGWIN*)
     # Add Windows-specific build commands here
