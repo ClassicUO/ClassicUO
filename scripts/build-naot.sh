ca51952193e65f0ab865cea6589fd0869dadd058
andreakarasho <andrea-angelini@outlook.it>
2024-01-14 01:51:18 +0100

    script fix

--- scripts/build-naot.sh
+++ scripts/build-naot.sh
@@ -1,2 +1,34 @@
-dotnet publish ../src/ClassicUO.Bootstrap/src/ClassicUO.Bootstrap.csproj -c Release -f net472 -o ../bin/dist
-dotnet publish ../src/ClassicUO.Client -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r win-x64 -o ../bin/dist
+#!/bin/bash
+
+set -e
+
+# Define paths and project details
+bootstrap_project="../src/ClassicUO.Bootstrap/src/ClassicUO.Bootstrap.csproj"
+client_project="../src/ClassicUO.Client"
+output_directory="../bin/dist"
+
+# Determine the platform
+platform=$(uname -s)
+
+# Build for the appropriate platform
+case $platform in
+  Linux)
+    # Add Linux-specific build commands here
+    dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory"
+    ;;
+  Darwin)
+    # Add macOS-specific build commands here
+    dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory"
+    ;;
+  MINGW* | CYGWIN*)
+    # Add Windows-specific build commands here
+    dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r win-x64 -o "$output_directory"
+    ;;
+  *)
+    echo "Unsupported platform: $platform"
+    exit 1
+    ;;
+esac
