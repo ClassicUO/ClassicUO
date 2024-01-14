ac0e0cffa52a093d7e92ae47cc2a24eed16af2c2
andreakarasho <andrea-angelini@outlook.it>
2024-01-14 14:20:13 +0100

    fixed a weird bug tht causes access mem violation

--- src/ClassicUO.Client/CUOEnviroment.cs
+++ src/ClassicUO.Client/CUOEnviroment.cs
@@ -53,7 +53,7 @@ namespace ClassicUO
 
         public static readonly bool IsUnix = Environment.OSVersion.Platform != PlatformID.Win32NT && Environment.OSVersion.Platform != PlatformID.Win32Windows && Environment.OSVersion.Platform != PlatformID.Win32S && Environment.OSVersion.Platform != PlatformID.WinCE;
 
-        public static readonly Version Version = Assembly.GetExecutingAssembly().GetName().Version;
+        public static readonly string Version = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "0.0.0.0";
         public static readonly string ExecutablePath =
 #if NETFRAMEWORK
            AppContext.BaseDirectory; // Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
