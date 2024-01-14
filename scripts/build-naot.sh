bdf0c9f13dad905f3161dc898de429c193cb6cb0
andreakarasho <andrea-angelini@outlook.it>
2024-01-14 01:51:18 +0100

    script fix

--- scripts/build-naot.sh
+++ scripts/build-naot.sh
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> script fix
 #!/bin/bash
 
 set -e
@@ -16,24 +19,40 @@ case $platform in
   Linux)
     # Add Linux-specific build commands here
     dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+<<<<<<< HEAD
     dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r linux-x64 -o "$output_directory" 
+=======
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory"
+>>>>>>> script fix
     ;;
   Darwin)
     # Add macOS-specific build commands here
     dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+<<<<<<< HEAD
     dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r osx-x64 -o "$output_directory" 
+=======
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -o "$output_directory"
+>>>>>>> script fix
     ;;
   MINGW* | CYGWIN*)
     # Add Windows-specific build commands here
     dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
+<<<<<<< HEAD
     dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r win-x64 -o "$output_directory" 
+=======
+    dotnet publish "$client_project" -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r win-x64 -o "$output_directory"
+>>>>>>> script fix
     ;;
   *)
     echo "Unsupported platform: $platform"
     exit 1
     ;;
+<<<<<<< HEAD
 esac
 =======
 dotnet publish ../src/ClassicUO.Bootstrap/src/ClassicUO.Bootstrap.csproj -c Release -f net472 -o ../bin/dist
 dotnet publish ../src/ClassicUO.Client -c Release -f net8.0 -p:PublishAot=true -p:TargetFrameworks=net8.0 -p:NativeLib=Shared -p:OutputType=Library -r win-x64 -o ../bin/dist
 >>>>>>> + classicuo.bootstrap app
+=======
+esac
+>>>>>>> script fix
