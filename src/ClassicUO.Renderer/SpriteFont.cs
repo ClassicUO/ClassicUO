9957c7f8a299cb0c0e6e4669f23da5c22d7cb3b7
andreakarasho <andrea-angelini@outlook.it>
2024-09-24 09:53:53 +0200

    + string -> readonlyspan<char>

--- src/ClassicUO.Renderer/SpriteFont.cs
+++ src/ClassicUO.Renderer/SpriteFont.cs
@@ -2,7 +2,7 @@
 
 // Copyright (c) 2024, andreakarasho
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 1. Redistributions of source code must retain the above copyright
@@ -16,7 +16,7 @@
 // 4. Neither the name of the copyright holder nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -79,7 +79,7 @@ namespace ClassicUO.Renderer
         internal List<char> CharacterMap { get; }
 
 
-        public Vector2 MeasureString(string text)
+        public Vector2 MeasureString(ReadOnlySpan<char> text)
         {
             if (text == null)
             {
