d6b73ac9cbc2daa94f1cd9800bddf5b3d6b098d5
andreakarasho <andrea-angelini@outlook.it>
2024-10-09 20:38:14 +0200

    + review the uint16converter class

--- src/ClassicUO.Utility/UInt16Converter.cs
+++ src/ClassicUO.Utility/UInt16Converter.cs
@@ -30,25 +30,30 @@
 
 #endregion
 
+using System;
 using System.Globalization;
 
 namespace ClassicUO.Utility
 {
     public static class UInt16Converter
     {
-        public static ushort Parse(string str)
+        public static ushort Parse(ReadOnlySpan<char> str)
         {
+            var style = NumberStyles.Integer;
             if (str.StartsWith("0x"))
             {
-                return ushort.Parse(str.Remove(0, 2), NumberStyles.HexNumber);
+                str = str.Slice(2);
+                style = NumberStyles.HexNumber;
             }
-
-            if (str.Length > 1 && str[0] == '-')
+            else if (str.Length > 1 && str[0] == '-')
             {
-                return (ushort) short.Parse(str);
+                if (short.TryParse(str, out var res))
+                {
+                    return (ushort)res;
+                }
             }
 
-            uint.TryParse(str, out uint v);
+            uint.TryParse(str, style, null, out uint v);
 
             return (ushort) v; // some server send 0xFFFF_FFFF in decimal form. C# doesn't like it. It needs a specific conversion
         }
