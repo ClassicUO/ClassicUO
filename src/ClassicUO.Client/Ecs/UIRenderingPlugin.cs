8e7b899573ca296e0c89ee98ef9ad30eee03475f
andreakarasho <andrea-angelini@outlook.it>
2025-01-25 01:16:01 +0100

    rename

--- src/ClassicUO.Client/Ecs/UIRenderingPlugin.cs
+++ src/ClassicUO.Client/Ecs/UIRenderingPlugin.cs
@@ -17,8 +17,8 @@ internal struct UIRenderingPlugin : IPlugin
             Res<AssetsServer> assets,
             Local<Texture2D> dumbTexture,
             Query<
-                Data<Bounds, InputState, ZIndex, UOInteractionWidget>,
-                Filter<With<Gui>, Optional<UOInteractionWidget>>> query) =>
+                Data<GuiBounds, GuiInputState, ZIndex, GuiUOInteractionWidget>,
+                Filter<With<Gui>, Optional<GuiUOInteractionWidget>>> query) =>
         {
             var b = batch.Value;
 
@@ -26,7 +26,7 @@ internal struct UIRenderingPlugin : IPlugin
             if (dumbTexture.Value == null)
             {
                 dumbTexture.Value = new Texture2D(b.GraphicsDevice, 1, 1);
-                dumbTexture.Value.SetData(new[] { Color.White });
+                dumbTexture.Value.SetData([Color.White]);
             }
 
             b.Begin(null, Matrix.Identity);
@@ -34,8 +34,8 @@ internal struct UIRenderingPlugin : IPlugin
             foreach ((var bounds, var inputState, var zIndex, var uoWidget) in query)
             {
                 var hue = inputState.Ref switch {
-                    InputState.Over => new Vector3(0x22, 1, 1f),
-                    InputState.Pressed => new Vector3(0x33, 1, 1f),
+                    GuiInputState.Over => new Vector3(0x22, 1, 1f),
+                    GuiInputState.Pressed => new Vector3(0x33, 1, 1f),
                     _ => Vector3.UnitZ
                 };
 
@@ -55,8 +55,8 @@ internal struct UIRenderingPlugin : IPlugin
                 else
                 {
                     var graphic = inputState.Ref switch {
-                        InputState.Over => uoWidget.Ref.Over,
-                        InputState.Pressed => uoWidget.Ref.Pressed,
+                        GuiInputState.Over => uoWidget.Ref.Over,
+                        GuiInputState.Pressed => uoWidget.Ref.Pressed,
                         _ => uoWidget.Ref.Normal
                     };
 
@@ -86,7 +86,7 @@ internal struct UIRenderingPlugin : IPlugin
         // input
         scheduler.AddSystem((
             World world,
-            Query<Data<Bounds, ZIndex, InputState, Children>, Filter<With<Gui>, Optional<Children>>> query,
+            Query<Data<GuiBounds, ZIndex, GuiInputState, Children>, Filter<With<Gui>, Optional<Children>>> query,
             // Query<Data<Bounds>, Filter<With<Gui>, Without<Parent>>> queryChildren,
             Res<MouseContext> mouseCtx,
             Local<EntityView?> selected) =>
@@ -103,14 +103,14 @@ internal struct UIRenderingPlugin : IPlugin
                 selected.Value = null;
             }
 
-            var newState = InputState.None;
+            var newState = GuiInputState.None;
             foreach ((var ent, var bounds, var zIndex, var inputState, var children) in query)
             {
                 if (selected.Value.HasValue)
                 {
                     if (selected.Value.Value == ent.Ref.ID && isDragging)
                     {
-                        newState = InputState.Pressed;
+                        newState = GuiInputState.Pressed;
 
                         if (length > 0)
                         {
@@ -126,7 +126,7 @@ internal struct UIRenderingPlugin : IPlugin
 
                                 foreach (var child in children)
                                 {
-                                    ref var childBounds = ref world.Get<Bounds>(child);
+                                    ref var childBounds = ref world.Get<GuiBounds>(child);
                                     childBounds.Value.X += (int)offset.X;
                                     childBounds.Value.Y += (int)offset.Y;
 
@@ -141,11 +141,11 @@ internal struct UIRenderingPlugin : IPlugin
                     if (!isDragging)
                     {
                         selected.Value = ent.Ref;
-                        newState = InputState.Over;
+                        newState = GuiInputState.Over;
                     }
                 }
 
-                inputState.Ref = InputState.None;
+                inputState.Ref = GuiInputState.None;
             }
 
             if (selected.Value.HasValue)
@@ -160,17 +160,17 @@ internal struct UIRenderingPlugin : IPlugin
                 => ecs.Entity()
                     .Add<Gui>()
                     .Set(new ZIndex())
-                    .Set(InputState.None)
-                    .Set(new Bounds() { Value = bounds ?? Rectangle.Empty});
+                    .Set(GuiInputState.None)
+                    .Set(new GuiBounds() { Value = bounds ?? Rectangle.Empty});
 
             var child3 = basicWidget(world, new() { X = 100, Y = 100, Width = 120, Height = 50 });
 
             var root = basicWidget(world, new () { X = 100, Y = 200, Width = 120, Height = 90 });
 
             var child0 = basicWidget(world)
-                .Set(new UOInteractionWidget() { Normal = 0x1589, Pressed = 0x158B, Over = 0x158A });
+                .Set(new GuiUOInteractionWidget() { Normal = 0x1589, Pressed = 0x158B, Over = 0x158A });
             var child1 = basicWidget(world)
-                .Set(new UOInteractionWidget() { Normal = 0x085d, Pressed = 0x085e, Over = 0x085f });
+                .Set(new GuiUOInteractionWidget() { Normal = 0x085d, Pressed = 0x085e, Over = 0x085f });
 
             root.AddChild(child0);
             root.AddChild(child1);
@@ -185,20 +185,20 @@ internal struct UIRenderingPlugin : IPlugin
 
 struct Gui;
 
-internal enum InputState
+internal enum GuiInputState
 {
     None,
     Over,
     Pressed,
 }
 
-struct Bounds { public Rectangle Value; }
-
-struct ChildOffset { public Vector2 Value; }
+struct GuiBounds { public Rectangle Value; }
 
 struct ZIndex { public float Value; }
 
-struct UOInteractionWidget
+struct GuiText { public string Value; }
+
+struct GuiUOInteractionWidget
 {
     public ushort Normal, Pressed, Over;
 }
