137a49460e6c733f67fded192df70354153b9679
TazmanianTad <bittiez@gmail.com>
2025-02-23 13:11:50 -0700

    Default to off

--- src/ClassicUO.Client/Configuration/Profile.cs
+++ src/ClassicUO.Client/Configuration/Profile.cs
@@ -309,7 +309,7 @@ namespace ClassicUO.Configuration
         public string WorldMapHiddenZoneFiles { get; set; } = string.Empty;
         public bool WorldMapShowGridIfZoomed { get; set; } = true;
         public bool WorldMapAllowPositionalTarget { get; set; } = false;
-        public bool ShowDPSWithDamageNumbers { get; set; } = true;
+        public bool ShowDPSWithDamageNumbers { get; set; } = false;
 
 
         public static uint GumpsVersion { get; private set; }
