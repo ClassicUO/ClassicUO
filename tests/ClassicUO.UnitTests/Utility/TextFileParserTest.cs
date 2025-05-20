8d0d46e80b311ebcafa910cdaf6c1ac0d204b4b0
andreakarasho <andrea-angelini@outlook.it>
2025-05-20 23:19:25 +0200

    fix test

--- tests/ClassicUO.UnitTests/Utility/TextFileParserTest.cs
+++ tests/ClassicUO.UnitTests/Utility/TextFileParserTest.cs
@@ -13,10 +13,10 @@ namespace ClassicUO.UnitTests.Utility
         [Fact]
         public void Parse_Long_Text()
         {
-            string s = " \"a # quoted\tstring\" a non quoted string    #ignoredstring  ";
+            string s = " {a # quoted\tstring} a non quoted string    #ignoredstring  ";
             char[] delimiters = new char[] { ' ', '\t' };
             char[] comments = new char[] { '#' };
-            char[] quotes = new char[] { '"', '"' };
+            char[] quotes = new char[] { '{', '}' };
             bool trim = true;
 
 
@@ -31,6 +31,32 @@ namespace ClassicUO.UnitTests.Utility
             Assert.Equal("non", tokens[2]);
             Assert.Equal("quoted", tokens[3]);
             Assert.Equal("string", tokens[4]);
+        }
+
+        [Fact]
+        public void Parse_Long_Text_SameQuotes()
+        {
+            string s = "1 2 3 4 @5 foo@6 bar@7@baz";
+            char[] delimiters = new char[] { ' ', '\t' };
+            char[] comments = new char[] { '#' };
+            char[] quotes = new char[] { '@', '@' };
+            bool trim = true;
+
+
+            ClassicUO.Utility.TextFileParser parser = new TextFileParser(s, delimiters, comments, quotes);
+
+            List<string> tokens = parser.ReadTokens(trim);
+
+            Assert.NotEmpty(tokens);
+            Assert.Equal(8, tokens.Count);
+            Assert.Equal("1", tokens[0]);
+            Assert.Equal("2", tokens[1]);
+            Assert.Equal("3", tokens[2]);
+            Assert.Equal("4", tokens[3]);
+            Assert.Equal("5 foo", tokens[4]);
+            Assert.Equal("6 bar", tokens[5]);
+            Assert.Equal("7", tokens[6]);
+            Assert.Equal("baz", tokens[7]);
         }
     }
 }
