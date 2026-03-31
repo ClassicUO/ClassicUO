4f5adad1e6ba33550e94a396dcb552066cac6d53
harrymunro <harry.j.munro@gmail.com>
2026-03-31 14:34:31 -0300

    Expand StackDataWriter tests with integer, ASCII, and round-trip coverage
    - Convert existing Assert.True to FluentAssertions
    - Add WriteUInt8, WriteBool, endianness, WriteZero tests
    - Add WriteASCII empty string and fixed-length padding tests
    - Add round-trip tests for UInt16/UInt32 LE and BE

--- tests/ClassicUO.UnitTests/IO/StackDataWriterTest.cs
+++ tests/ClassicUO.UnitTests/IO/StackDataWriterTest.cs
@@ -1,10 +1,8 @@
-﻿using ClassicUO.IO;
+using ClassicUO.IO;
+using FluentAssertions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Buffers.Binary;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace ClassicUO.UnitTests.IO
@@ -30,11 +28,11 @@ namespace ClassicUO.UnitTests.IO
             Span<char> span = stackalloc char[str.Length + 1]; // '\0'
             str.AsSpan().CopyTo(span);
 
-            Assert.True(MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten)));
+            MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten))
+                .Should().BeTrue();
 
             writer.Dispose();
         }
-
 
         [Fact]
         public void Write_BigEndian_String_Greater_Fixed_Size_Than_RealString()
@@ -56,7 +54,8 @@ namespace ClassicUO.UnitTests.IO
             Span<char> span = stackalloc char[size];
             str.AsSpan().CopyTo(span);
 
-            Assert.True(MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten)));
+            MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten))
+                .Should().BeTrue();
 
             writer.Dispose();
         }
@@ -81,7 +80,8 @@ namespace ClassicUO.UnitTests.IO
             Span<char> span = stackalloc char[size];
             str.AsSpan(0, size).CopyTo(span);
 
-            Assert.True(MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten)));
+            MemoryMarshal.AsBytes(span).SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten))
+                .Should().BeTrue();
 
             writer.Dispose();
         }
@@ -95,7 +95,220 @@ namespace ClassicUO.UnitTests.IO
 
             writer.WriteASCII(a);
 
-            Assert.True(b.AsSpan().SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten)));
+            b.AsSpan().SequenceEqual(writer.Buffer.Slice(0, writer.BytesWritten))
+                .Should().BeTrue();
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData((byte)0x00)]
+        [InlineData((byte)0x7F)]
+        [InlineData((byte)0xFF)]
+        public void WriteUInt8_WritesCorrectByte(byte value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteUInt8(value);
+
+            writer.BytesWritten.Should().Be(1);
+            writer.Buffer[0].Should().Be(value);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(true, (byte)0x01)]
+        [InlineData(false, (byte)0x00)]
+        public void WriteBool_WritesCorrectByte(bool value, byte expected)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteBool(value);
+
+            writer.BytesWritten.Should().Be(1);
+            writer.Buffer[0].Should().Be(expected);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData((ushort)0x0102)]
+        [InlineData((ushort)0xFF00)]
+        [InlineData((ushort)0x0000)]
+        public void WriteUInt16LE_WritesLittleEndian(ushort value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteUInt16LE(value);
+
+            writer.BytesWritten.Should().Be(2);
+            writer.Buffer[0].Should().Be((byte)(value & 0xFF));
+            writer.Buffer[1].Should().Be((byte)(value >> 8));
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData((ushort)0x0102)]
+        [InlineData((ushort)0xFF00)]
+        [InlineData((ushort)0x0000)]
+        public void WriteUInt16BE_WritesBigEndian(ushort value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteUInt16BE(value);
+
+            writer.BytesWritten.Should().Be(2);
+            writer.Buffer[0].Should().Be((byte)(value >> 8));
+            writer.Buffer[1].Should().Be((byte)(value & 0xFF));
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(0x01020304u)]
+        [InlineData(0xFF000000u)]
+        [InlineData(0x00000000u)]
+        public void WriteUInt32LE_WritesLittleEndian(uint value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteUInt32LE(value);
+
+            writer.BytesWritten.Should().Be(4);
+            writer.Buffer[0].Should().Be((byte)(value & 0xFF));
+            writer.Buffer[1].Should().Be((byte)((value >> 8) & 0xFF));
+            writer.Buffer[2].Should().Be((byte)((value >> 16) & 0xFF));
+            writer.Buffer[3].Should().Be((byte)((value >> 24) & 0xFF));
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(0x01020304u)]
+        [InlineData(0xFF000000u)]
+        [InlineData(0x00000000u)]
+        public void WriteUInt32BE_WritesBigEndian(uint value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteUInt32BE(value);
+
+            writer.BytesWritten.Should().Be(4);
+            writer.Buffer[0].Should().Be((byte)((value >> 24) & 0xFF));
+            writer.Buffer[1].Should().Be((byte)((value >> 16) & 0xFF));
+            writer.Buffer[2].Should().Be((byte)((value >> 8) & 0xFF));
+            writer.Buffer[3].Should().Be((byte)(value & 0xFF));
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(16)]
+        public void WriteZero_WritesCorrectNumberOfZeros(int count)
+        {
+            StackDataWriter writer = new StackDataWriter(32);
+
+            writer.WriteZero(count);
+
+            writer.BytesWritten.Should().Be(count);
+            for (int i = 0; i < count; i++)
+            {
+                writer.Buffer[i].Should().Be(0);
+            }
+
+            writer.Dispose();
+        }
+
+        [Fact]
+        public void WriteASCII_EmptyString_WritesOnlyNullTerminator()
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+
+            writer.WriteASCII("");
+
+            writer.BytesWritten.Should().Be(1);
+            writer.Buffer[0].Should().Be(0x00);
+
+            writer.Dispose();
+        }
+
+        [Fact]
+        public void WriteASCII_FixedLength_PadsWithZeros()
+        {
+            StackDataWriter writer = new StackDataWriter(32);
+
+            writer.WriteASCII("AB", 5);
+
+            writer.BytesWritten.Should().Be(5);
+            writer.Buffer[0].Should().Be(0x41); // 'A'
+            writer.Buffer[1].Should().Be(0x42); // 'B'
+            writer.Buffer[2].Should().Be(0x00);
+            writer.Buffer[3].Should().Be(0x00);
+            writer.Buffer[4].Should().Be(0x00);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData((ushort)0x1234)]
+        [InlineData((ushort)0x0000)]
+        [InlineData((ushort)0xFFFF)]
+        public void RoundTrip_UInt16LE(ushort value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+            writer.WriteUInt16LE(value);
+
+            var reader = new StackDataReader(writer.Buffer);
+            reader.ReadUInt16LE().Should().Be(value);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(0x12345678u)]
+        [InlineData(0x00000000u)]
+        [InlineData(0xFFFFFFFFu)]
+        public void RoundTrip_UInt32LE(uint value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+            writer.WriteUInt32LE(value);
+
+            var reader = new StackDataReader(writer.Buffer);
+            reader.ReadUInt32LE().Should().Be(value);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData((ushort)0x1234)]
+        [InlineData((ushort)0x0000)]
+        [InlineData((ushort)0xFFFF)]
+        public void RoundTrip_UInt16BE(ushort value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+            writer.WriteUInt16BE(value);
+
+            var reader = new StackDataReader(writer.Buffer);
+            reader.ReadUInt16BE().Should().Be(value);
+
+            writer.Dispose();
+        }
+
+        [Theory]
+        [InlineData(0x12345678u)]
+        [InlineData(0x00000000u)]
+        [InlineData(0xFFFFFFFFu)]
+        public void RoundTrip_UInt32BE(uint value)
+        {
+            StackDataWriter writer = new StackDataWriter(8);
+            writer.WriteUInt32BE(value);
+
+            var reader = new StackDataReader(writer.Buffer);
+            reader.ReadUInt32BE().Should().Be(value);
 
             writer.Dispose();
         }
