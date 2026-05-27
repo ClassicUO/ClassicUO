c9e2276440ab220110b40043e0091472919dbb28
andreakarasho <andrea-angelini@outlook.it>
2026-05-27 23:41:30 +0200

    build(ecs): native AOT publish for cuo-ecs
    - build-naot.sh: publish ClassicUO.Ecs to bin/dist-ecs
    - csproj: copy native deps (SDL3, FNA3D, Clay, ...) to PublishDir after publish

--- scripts/build-naot.sh
+++ scripts/build-naot.sh
@@ -5,7 +5,9 @@ set -e
 # Define paths and project details
 bootstrap_project="../src/ClassicUO.Bootstrap/src/ClassicUO.Bootstrap.csproj"
 client_project="../src/ClassicUO.Client"
+ecs_project="../src/ClassicUO.Ecs/ClassicUO.Ecs.csproj"
 output_directory="../bin/dist"
+ecs_output_directory="../bin/dist-ecs"
 target=""
 
 # Determine the platform
@@ -34,3 +36,4 @@ esac
 
 dotnet publish "$bootstrap_project" -c Release -o "$output_directory"
 dotnet publish "$client_project" -c Release -p:NativeLib=Shared -p:OutputType=Library -r $target -o "$output_directory"
+dotnet publish "$ecs_project" -c Release -r $target -o "$ecs_output_directory"
